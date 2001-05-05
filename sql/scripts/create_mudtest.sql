09db24c2aa06f9c00d4aec634dc69e46a7384c46
karchan <karchan@1239004b-fd02-0410-9f86-fb9af6f58458>
2001-05-05 16:48:37 +0000

    /* added some comments. Also put this thing into the crontab to be run once a week, on Sunday, at noon */

--- sql/scripts/create_mudtest.sql
+++ sql/scripts/create_mudtest.sql
@@ -4,12 +4,14 @@ cd /karchan/mud/sql
 
 . ./mysql_constants
 
+# First drop the database mudtest and recreate it to make sure that it is empty
 ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s <<END_OF_DATA
 drop database mudtest;
 create database mudtest;
 
 END_OF_DATA
 
+# Dump all data from database "mud" to database "mudtest"
 ${MYSQL_DUMP} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} ${MYSQL_DB} | \
 ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL_TESTDB}
 
