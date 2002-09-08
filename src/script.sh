f1d802696306d30f97983fbcee9316331d416966
karchan <karchan@1239004b-fd02-0410-9f86-fb9af6f58458>
2002-09-08 15:57:10 +0000

    /* added a echo redirect to audit.trail so I can see if the mmserver has restarted */

--- src/script.sh
+++ src/script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo "Started"
+echo "Started" >>/karchan/mud/data/audit.trail
 while (sleep 10) do
 	./mmserver > ./nohup.out
 done
 echo "Stopped"
+echo "Stopped" >>/karchan/mud/data/audit.trail
+
