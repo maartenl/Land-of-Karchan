b2fb5b2122ccff384a5c28f1e2703cd1ff20a81c
karchan <karchan@1239004b-fd02-0410-9f86-fb9af6f58458>
2002-12-27 16:48:45 +0000

    /* also put message into /karchan/mud/data/bigfile. That way I can see what the last issued command was */

--- src/script.sh
+++ src/script.sh
@@ -2,8 +2,12 @@
 
 echo "Started"
 echo "Started" >>/karchan/mud/data/audit.trail
+echo "Started" >>/karchan/mud/data/bigfile
 ./mmserver
 while (sleep 10) do
+	echo "Restarted"
+	echo "Restarted" >>/karchan/mud/data/audit.trail
+	echo "Restarted" >>/karchan/mud/data/bigfile
 	./mmserver
 done
 echo "Stopped"
