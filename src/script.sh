635ed21a98c820d07204d4f63f6ea3ee207e31ad
karchan <karchan@1239004b-fd02-0410-9f86-fb9af6f58458>
2002-12-27 16:47:10 +0000

    /* no more waiting periods. Downtime is BAD */

--- src/script.sh
+++ src/script.sh
@@ -2,8 +2,9 @@
 
 echo "Started"
 echo "Started" >>/karchan/mud/data/audit.trail
+./mmserver
 while (sleep 10) do
-	./mmserver > ./nohup.out
+	./mmserver
 done
 echo "Stopped"
 echo "Stopped" >>/karchan/mud/data/audit.trail
