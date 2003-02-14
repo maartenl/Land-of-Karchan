3c12d18466543ce09743675a85cfb5e2c61454b6
karchan <karchan@1239004b-fd02-0410-9f86-fb9af6f58458>
2003-02-14 23:17:37 +0000

    /* fixed a rather nasty bug where I entered in a NULL value to the parser in the hope that things would work out.
    Apparently they didn't, and now I create a (slightly bogus) mudpersonstruct
    structure to provide to the parser.
    
    It seems to work. */

--- src/events.c
+++ src/events.c
@@ -26,6 +26,7 @@ maarten_l@yahoo.com
 -------------------------------------------------------------------------*/
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 #include "typedefs.h"
 #include "parser.h"
 
@@ -33,6 +34,47 @@ maarten_l@yahoo.com
 	\brief  simple executable that starts up events depending on information 
 in the database table "events". Will start up "methods" in the "methods" 
 table. Usually this executable is put into your crontab. */
+
+//! tokenizes the main command into seperate words
+/*! tokenises the main command into separate words, space (" ") is used
+    as the separator. Multiple spaces are interpreted as a single space.
+    The way this works is that elements of the tokenarray will point to
+    the start of the new words in the 'commandstring'. This means that
+    the commandstring contains a string with multiple \0 breaks.
+    The first token (tokenarray[0]) will always point to the first word
+    in the commandstring, which is always the same address as commandstring.
+    <P>
+    Why not strtok? Strtok is dangerous and is not thread safe.
+\param commandstring char* containing the command. Attention! This variable
+will be changed!!!
+\param tokenarray char** containing the different words, will never exceed 50 words.
+\return int containing the amount of tokens entered into the array
+*/
+int tokenizer(char *commandstring, char **tokenarray)
+{
+	char *index;
+	int number;
+	tokenarray[0] = commandstring;
+	number = 1;
+	index = commandstring;
+	while (*index != 0)
+	{
+		if (*index == ' ')
+		{
+			*index=0;
+			if ((*(index+1) != ' ') && (*(index+1) != 0))
+			{
+				tokenarray[number++] = index+1;
+				if (number == 50)
+				{
+					return number;
+				}
+			}
+		}
+		index++;
+	}
+	return number;
+}
 
 int 
 main(int argc, char *argv[])
@@ -64,8 +106,24 @@ main(int argc, char *argv[])
 		row = mysql_fetch_row(res);
 		while (row != NULL)
 		{
+	      mudpersonstruct mypersonstruct;
+			mypersonstruct.readbuf=NULL;
+			strcpy(mypersonstruct.name,"anonymous");
+			strcpy(mypersonstruct.password,"bogus");
+			strcpy(mypersonstruct.address,"asd");
+			strcpy(mypersonstruct.cookie, "bogus");
+			mypersonstruct.frames=1;
+			mypersonstruct.action = "mud";
+			mypersonstruct.command = row[0];
+	      mypersonstruct.socketfd = 0;
+			mypersonstruct.newchar=NULL;
+			mypersonstruct.memblock=strdup(mypersonstruct.command);
+			mypersonstruct.tokenamount = tokenizer(mypersonstruct.memblock, mypersonstruct.tokens);
+			mypersonstruct.next=NULL;
 			room =  atoi(row[1]);
-			Parse(NULL, "anonymous", &room, "this is totally bogus stuff", row[0], 1);
+			mypersonstruct.room=room;
+			Parse(&mypersonstruct, "anonymous", &room, "this is totally bogus stuff", row[0], 1);
+			free(mypersonstruct.memblock);
 			row = mysql_fetch_row(res);
 		}
 		mysql_free_result(res);
