e5c18c0832f4138ede290b486cfd28c258e0743b
karn <karn@1239004b-fd02-0410-9f86-fb9af6f58458>
2003-12-02 21:53:30 +0000

    /* some more php scripts to replace the cgi ones. */

---
+++ scripts/charactersheet.php
@@ -0,0 +1,130 @@
+<?php
+/*-------------------------------------------------------------------------
+cvsinfo: $Header$
+Maarten's Mud, WWW-based MUD using MYSQL
+Copyright (C) 1998  Maarten van Leunen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Maarten van Leunen
+Appelhof 27
+5345 KA Oss
+Nederland
+Europe
+maarten_l@yahoo.com
+-------------------------------------------------------------------------*/
+?>
+<HTML>
+<HEAD>
+<TITLE>
+Land of Karchan - <?php echo $_REQUEST{"name"} ?>
+</TITLE>
+</HEAD>
+                                                                                                                      
+<BODY>
+<BODY BGCOLOR=#FFFFFF BACKGROUND="/images/gif/webpic/back4.gif">
+<H1>
+<IMG SRC="/images/gif/dragon.gif">
+Character Sheet of <?php echo $_REQUEST{"name"} ?></H1>
+<HR>
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/scripts/connect.php"; 
+
+$result = mysql_query("select usertable.name, title, sex,
+	concat(age,
+	if(length = 'none', '', concat(', ',length)),
+        if(width = 'none', '', concat(', ',width)),
+        if(complexion = 'none', '', concat(', ',complexion)),
+        if(eyes = 'none', '', concat(', ',eyes)),
+        if(face = 'none', '', concat(', ',face)),
+        if(hair = 'none', '', concat(', ',hair)),
+        if(beard = 'none', '', concat(', ',beard)),
+        if(arm = 'none', '', concat(', ',arm)),
+        if(leg = 'none', '', concat(', ',leg)),
+        ' ', sex, ' ', race), 
+        concat('<IMG SRC=\"',imageurl,'\">'), 
+        guild, 
+        concat('<A HREF=\"',homepageurl,'\">',homepageurl,'</A>'), 
+        \"Yes\", dateofbirth, cityofbirth, usertable.lastlogin, storyline 
+        from usertable, characterinfo 
+        where usertable.name = '".$_REQUEST{"name"}."' and 
+        usertable.name = characterinfo.name"
+	, $dbhandle)
+	or die("Query failed : " . mysql_error());
+if (mysql_num_rows($result) == 0)
+{
+	die("Character does not exist.");
+}
+if ($myrow = mysql_fetch_row($result)) 
+{
+	// full character sheet 
+	printf("<B>Name:</B> %s<BR>", $myrow[0]);
+	printf("<B>Title:</B> %s<BR>", $myrow[1]);
+	printf("<B>Sex:</B> %s<BR>", $myrow[2]);
+	printf("<B>Description:</B> %s<BR>", $myrow[3]);
+	if (($myrow[4] != "<IMG SRC=\"\">") &&
+	   ($myrow[4] != "<IMG SRC=\"http://\">") )
+	{
+		printf("<B>Image:</B> %s<BR>", $myrow[4]);
+	}
+	printf("<B>Member of:</B> %s<BR>", $myrow[5]);
+	if (($myrow[6] != "<A HREF=\"\"></A>") &&
+	   ($myrow[6] != "<A HREF=\"http://\">http://</A>") )
+	{
+		printf("<B>Homepage:</B> %s<BR>", $myrow[6]);
+	}
+	printf("<B>Born:</B> %s<BR>", $myrow[7]);
+	printf("<B>Born When:</B> %s<BR>", $myrow[8]);
+	printf("<B>Born Where:</B> %s<BR>", $myrow[9]);
+	familyValues($dbhandle);
+	printf("<B>Last logged on:</B> %s<BR>", $myrow[10]);
+	printf("<B>Storyline:</B> %s<BR>", $myrow[11]);
+}
+mysql_close($dbhandle);
+
+function familyValues($arg)
+{
+	$result = mysql_query("select familyvalues.description, toname,
+		characterinfo.name 
+		from family, familyvalues 
+		left join characterinfo 
+		on characterinfo.name = family.toname 
+		where family.name = '".$_REQUEST{"name"}."' and 
+		family.description = familyvalues.id", $arg)
+		or die("Query failed : " . mysql_error());
+	printf("<B>Family Relations:</B><BR><UL>");
+	while ($myrow = mysql_fetch_row($result))
+	{
+		if ($myrow[2] == null)
+		{
+			printf("<LI>%s of %s<BR>", $myrow[0], $myrow[1]);
+		}
+		else
+		{
+			printf("<LI>%s of <A
+HREF=\"/scripts/charactersheet.php?name=%s\">%s</A><BR>",
+				$myrow[0],$myrow[1],$myrow[1]);
+		}
+	}
+	printf("</UL>");
+}
+?>
+<p>
+<a HREF="/scripts/charactersheets.php">
+<img SRC="/images/gif/webpic/buttono.gif"  
+BORDER="0"></a><p>
+
+</BODY>
+</HTML>
---
+++ scripts/editcharactersheet.php
@@ -0,0 +1,136 @@
+<?php
+/*-------------------------------------------------------------------------
+cvsinfo: $Header$
+Maarten's Mud, WWW-based MUD using MYSQL
+Copyright (C) 1998  Maarten van Leunen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Maarten van Leunen
+Appelhof 27
+5345 KA Oss
+Nederland
+Europe
+maarten_l@yahoo.com
+-------------------------------------------------------------------------*/
+?>
+<HTML>
+<HEAD>
+<TITLE>
+Land of Karchan - <?php echo $_REQUEST{"name"} ?>
+</TITLE>
+</HEAD>
+                                                                                                                      
+<BODY>
+<BODY BGCOLOR=#FFFFFF BACKGROUND="/images/gif/webpic/back4.gif">
+<H1>
+<IMG SRC="/images/gif/dragon.gif">
+Edit Character Sheet of <?php echo $_REQUEST{"name"} ?></H1>
+
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/scripts/connect.php"; 
+
+
+$result = mysql_query("select * from usertable where usertable.name =
+	'".$_REQUEST{"name"}."' 
+	and usertable.password = '".$_REQUEST{"password"}."'"
+	, $dbhandle)
+	or die("Query failed : " . mysql_error());
+if (mysql_num_rows($result) == 0)
+{
+	die("Password not correct, or character does not exist.");
+}
+$result = mysql_query("select *, replace(replace(replace(storyline,
+	'&','&amp;'),'<', '&lt;'), '>', '&gt;') 
+	from characterinfo 
+        where name = '".$_REQUEST{"name"}."'", $dbhandle)
+	or die("Query failed : " . mysql_error());
+if ($myrow = mysql_fetch_row($result)) 
+{
+	// full character sheet 
+?>
+<FORM METHOD="GET" ACTION="/cgi-bin/submitcharactersheet.cgi">
+<HR>
+Homepage Url:<BR>
+<INPUT TYPE="text" NAME="homepageurl" VALUE="<?php echo $myrow[2] ?>" SIZE="50" MAXLENGTH="253"><P>
+Image Url:<BR>
+<INPUT TYPE="text" NAME="imageurl" VALUE="<?php echo $myrow[1] ?>" SIZE="50" MAXLENGTH="253"><P>
+Date of Birth: 
+<INPUT TYPE="text" NAME="dateofbirth" VALUE="<?php echo $myrow[3] ?>" SIZE="20" MAXLENGTH="253"><P>
+City of Birth:
+<INPUT TYPE="text" NAME="cityofbirth" VALUE="<?php echo $myrow[4] ?>" SIZE="20" MAXLENGTH="253"><P>
+Original storyline was...<BR><TABLE><TR><TD><TT><?php echo $myrow[6] ?></TT></TD></TR></TABLE>
+Storyline:<BR>
+<TEXTAREA NAME="storyline" VALUE="" ROWS="30" COLS="85"></TEXTAREA><P>
+<?php familyValues($dbhandle); ?>
+<INPUT TYPE="hidden" NAME="name" VALUE="<?php echo $_REQUEST{"name"} ?>">
+<INPUT TYPE="hidden" NAME="password" VALUE="<?php echo $_REQUEST{"password"} ?>">
+<INPUT TYPE="submit" VALUE="Submit">
+<INPUT TYPE="reset" VALUE="Clear">
+</FORM>
+<?php
+}
+else
+{
+	// empty character sheet
+?>
+<FORM METHOD="GET" ACTION="/cgi-bin/submitcharactersheet.cgi">
+<HR>
+Homepage Url:<BR>
+<INPUT TYPE="text" NAME="homepageurl" VALUE="http://" SIZE="50" MAXLENGTH="253"><P>
+Image Url:<BR>
+<INPUT TYPE="text" NAME="imageurl" VALUE="http://" SIZE="50" MAXLENGTH="253"><P>
+Date of Birth: 
+<INPUT TYPE="text" NAME="dateofbirth" VALUE="" SIZE="20" MAXLENGTH="253"><P>
+City of Birth:
+<INPUT TYPE="text" NAME="cityofbirth" VALUE="" SIZE="20" MAXLENGTH="253"><P>
+Storyline:<BR>
+<TEXTAREA NAME="storyline" VALUE="" ROWS="30" COLS="85"></TEXTAREA><P>
+<?php familyValues($dbhandle); ?>
+<INPUT TYPE="hidden" NAME="name" VALUE="<?php echo $_REQUEST{"name"} ?>">
+<INPUT TYPE="hidden" NAME="password" VALUE="<?php echo $_REQUEST{"password"} ?>">
+<INPUT TYPE="submit" VALUE="Submit">
+<INPUT TYPE="reset" VALUE="Clear">
+</FORM>
+<?php
+}
+mysql_close($dbhandle);
+
+function familyValues($arg)
+{
+	$result = mysql_query("select * from familyvalues", $arg)
+		or die("Query failed : " . mysql_error());
+	printf("(<I>Make sure you spell the name of the ");
+	printf("familymember correctly, otherwise the corresponding ");
+	printf("charactersheet can not be obtained.</I>)<BR>");
+	printf("Add family relation:\r\n");
+	printf("<SELECT NAME=\"family\">\r\n");
+	printf("<option selected value=\"0\">None");
+	while ($myrow = mysql_fetch_row($result))
+	{
+		printf("<option selected value=\"%s\">%s", 
+			$myrow[0],$myrow[1]);
+	}
+	printf("</SELECT>of ");
+	printf("<INPUT TYPE=\"text\" NAME=\"familyname\" VALUE=\"\" SIZE=\"50\" MAXLENGTH=\"40\"><P>");
+}
+?>
+<p>
+<a HREF="/scripts/charactersheets.php">
+<img SRC="/images/gif/webpic/buttono.gif"  
+BORDER="0"></a><p>
+
+</BODY>
+</HTML>
