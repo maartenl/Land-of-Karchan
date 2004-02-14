d8bd1ace9b47e82efe807be6e2a289f097768868
karn <karn@1239004b-fd02-0410-9f86-fb9af6f58458>
2004-02-14 10:47:47 +0000

    /* php scripts espescially designed to interface with the java mud server. Basically a pass-through scripts between the apache server and the java mud server . */

---
+++ scripts/mudlogon.php
@@ -0,0 +1,83 @@
+<?
+/*-------------------------------------------------------------------------
+cvsinfo: $Header$
+Maarten's Mud, WWW-based MUD using MYSQL
+Copyright (C) 1998  Maarten van Leunen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Maarten van Leunen
+Appelhof 27
+5345 KA Oss
+Nederland
+Europe
+maarten_l@yahoo.com
+-------------------------------------------------------------------------*/
+?>
+<?php
+	header ("Content-type: text/html");
+	
+// Hack prevention.
+//	$headers = apache_request_headers();
+//	header()
+//	setcookie();
+	ob_start();
+
+//	foreach ($headers as $header => $value) 
+//	{
+//		echo "$header: $value <br />\n";
+//	}
+	$fp = fsockopen ("localhost", 3339, $errno, $errstr, 30);
+	if (!$fp) 
+	{
+		echo "$errstr ($errno)<br>\n";
+	} 
+	else 
+	{
+		fgets ($fp,128); // Mmud id
+		fgets ($fp,128); // action
+		fputs ($fp, "logon\n");
+		fgets ($fp,128); // name
+		fputs ($fp, $_REQUEST{"name"}."\n");
+		fgets ($fp,128); // password
+		fputs ($fp, $_REQUEST{"password"}."\n");
+		fgets ($fp,128); // cookie
+		fputs ($fp, $_COOKIE["Karchan"]."\n");
+		fgets ($fp,128); // frames
+		fputs ($fp, $_REQUEST{"frames"}."\n");
+		// retrieve cookie that is always sent when attempting a login.
+		$cookie = fgets ($fp,128);
+		if (strstr($cookie, "sessionpassword=") != FALSE)
+		{
+			$cookie = substr($cookie, 16);
+			$cookie = substr_replace($cookie, "", -1, 1);
+			setcookie("Karchan", $cookie);
+		}
+		else
+		{
+			echo $cookie;
+		}
+		$readline = fgets ($fp,128);
+		while ((!feof($fp)) && ($readline != ".\n"))
+		{
+			echo $readline;
+			$readline = fgets ($fp,128);
+		}
+  		fputs ($fp, "\nOk\nOk\n");
+		fclose ($fp);
+	}
+	ob_end_flush();
+	exit;
+?>
