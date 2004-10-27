a275d889c15f44f960ae81057c8cae4f8e8a771c
karn <karn@1239004b-fd02-0410-9f86-fb9af6f58458>
2004-10-27 19:35:17 +0000

    /* added the possibility to change and add events, commands and methods. Also improved on the help. */

--- scripts/admin_commandseventsmethods.php
+++ scripts/admin_commandseventsmethods.php
@@ -43,6 +43,183 @@ Commands, Events and Methods</H1>
 include $_SERVER['DOCUMENT_ROOT']."/scripts/connect.php"; 
 include $_SERVER['DOCUMENT_ROOT']."/scripts/admin_authorize.php";
 
+/* the following constraints need to be checked before any kind of update is
+to take place:
+
+changing event:
+- check that event is owned by owner or is null
+- check callable is either 1 or 0
+- check that methodname exists
+- check that name of character (if filled in) exists
+- check that room number (if filled in) exists
+- check that numbers are filled in for times and room
+add event:
+- check that method exists
+
+changing command:
+add command:
+- check that method exists
+
+changing method:
+- first check that the change is approved (i.e. owner or null)
+adding method:
+- no check
+
+/* 
+------------------------------------------------------------------
+EVENTS
+------------------------------------------------------------------
+*/
+if ( (isset($_REQUEST{"eventid"}))
+	&& (isset($_REQUEST{"eventcharname"}))
+    && isset($_REQUEST{"eventroom"})
+    && isset($_REQUEST{"eventmonth"})
+    && isset($_REQUEST{"eventdayofmonth"})
+    && isset($_REQUEST{"eventhour"})
+    && isset($_REQUEST{"eventminute"})
+    && isset($_REQUEST{"eventdayofweek"})
+    && isset($_REQUEST{"eventmethodname"})
+    && isset($_REQUEST{"eventcallable"}) )
+{
+	// check proper information formats
+	if ( !is_numeric($_REQUEST{"eventid"}) ||
+         !is_numeric($_REQUEST{"eventmonth"}) ||
+         !is_numeric($_REQUEST{"eventdayofmonth"}) ||
+         !is_numeric($_REQUEST{"eventhour"}) ||
+         !is_numeric($_REQUEST{"eventminute"}) ||
+         !is_numeric($_REQUEST{"eventdayofweek"}) )
+	{
+		die("Expected one of the fields to be an integer, and it wasn't.");
+	}
+	// check that the event is the proper owner.
+    // check it.
+    $result = mysql_query("select eventid from mm_events where eventid = \"".
+        mysql_escape_string($_REQUEST{"eventid"}).  
+        "\" and (owner is null or owner = \"".   
+        mysql_escape_string($_COOKIE["karchanadminname"]).
+        "\")"
+        , $dbhandle)
+        or die("Query(1) failed : " . mysql_error());
+    if (mysql_num_rows($result) != 1)
+    {
+        die("You are not the owner of this event.");
+    }
+	// check to see that method exists
+	$result = mysql_query("select 1 from mm_methods where name = \"".
+		mysql_escape_string($_REQUEST{"eventmethodname"}).
+		"\""
+		, $dbhandle)
+		or die("Query failed : " . mysql_error());
+	if (mysql_num_rows($result) != 1)
+	{
+		die("Method does not exist.");
+	}
+    if ( ($_REQUEST{"eventcallable"} != "1") &&
+        ($_REQUEST{"eventcallable"} != "0") )
+    {
+        die("Callable should be either 0 or 1.");
+    }
+	// check to see that character exists
+	if (trim($_REQUEST{"eventcharname"}) != "")
+	{
+		$result = mysql_query("select 1 from mm_usertable where name = \"".
+			mysql_escape_string($_REQUEST{"eventcharname"}).
+			"\""
+			, $dbhandle)
+			or die("Query failed : " . mysql_error());
+		if (mysql_num_rows($result) != 1)
+		{
+			die("Character ".$_REQUEST{"eventcharname"}." does not exist.");
+		}
+	}
+	// check to see that room exists
+	if (trim($_REQUEST{"eventroom"}) != "")
+	{
+		$result = mysql_query("select 1 from mm_rooms where id = \"".
+			mysql_escape_string($_REQUEST{"eventroom"}).
+			"\""
+			, $dbhandle)
+			or die("Query failed : " . mysql_error());
+		if (mysql_num_rows($result) != 1)
+		{
+			die("Room ".$_REQUEST{"eventroom"}." does not exist.");
+		}
+	}
+	// make that change
+	$query = "update mm_events set name=".
+		(trim($_REQUEST{"eventcharname"}) != "" ?
+			"\"".mysql_escape_string($_REQUEST{"eventcharname"})."\"":
+			"null").
+		", month=\"".
+		mysql_escape_string($_REQUEST{"eventmonth"}).
+		"\", dayofmonth=\"".
+		mysql_escape_string($_REQUEST{"eventdayofmonth"}).
+		"\", hour=\"".
+		mysql_escape_string($_REQUEST{"eventhour"}).
+		"\", minute=\"".
+		mysql_escape_string($_REQUEST{"eventminute"}).
+		"\", dayofweek=\"".
+		mysql_escape_string($_REQUEST{"eventdayofweek"}).
+		"\", callable=\"".
+		mysql_escape_string($_REQUEST{"eventcallable"}).
+		"\", method_name=\"".
+		mysql_escape_string($_REQUEST{"eventmethodname"}).
+		"\", room=".
+		(trim($_REQUEST{"eventroom"}) != "" ?
+			"\"".mysql_escape_string($_REQUEST{"eventroom"})."\"":
+			"null").
+		", owner=\"".
+		mysql_escape_string($_COOKIE["karchanadminname"]).
+		"\" where eventid = \"".
+		mysql_escape_string($_REQUEST{"eventid"}).
+		"\"";
+    mysql_query($query
+        , $dbhandle)
+        or die("Query(8) failed : " . mysql_error());
+    writeLogLong($dbhandle, "Changed event ".$_REQUEST{"eventid"}.
+		".", $query);
+}
+if (isset($_REQUEST{"addeventmethodname"}))
+{
+	// check to see that method exists
+	$result = mysql_query("select 1 from mm_methods where name = \"".
+		mysql_escape_string($_REQUEST{"addeventmethodname"}).
+		"\""
+		, $dbhandle)
+		or die("Query failed : " . mysql_error());
+	if (mysql_num_rows($result) != 1)
+	{
+		die("Method does not exist.");
+	}
+	// create new id
+	$result = mysql_query("select max(eventid) as id from mm_events"
+		, $dbhandle)
+		or die("Query failed : " . mysql_error());
+	$maxid = 0;
+	while ($myrow = mysql_fetch_array($result)) 
+	{
+		$maxid = $myrow["id"];
+	}
+	if ($maxid == 0)
+	{
+		die("Could not compute id for event.");
+	}
+	
+	// make that change
+	$query = "insert into mm_events (eventid, method_name, owner) values(".
+		($maxid + 1).
+		", \"".
+		mysql_escape_string($_REQUEST{"addeventmethodname"}).
+		"\", \"".
+		mysql_escape_string($_COOKIE["karchanadminname"]).
+		"\")";
+    mysql_query($query
+        , $dbhandle)
+        or die("Query(8) failed : " . mysql_error());
+    writeLogLong($dbhandle, "Added event ".($maxid + 1).
+		" which uses method ".$_REQUEST{"addeventmethodname"}.".", $query);
+}
+
 $result = mysql_query("select date_format(now(), \"%Y-%m-%d %T\") as now"
 	, $dbhandle)
 	or die("Query failed : " . mysql_error());
@@ -59,7 +236,18 @@ $result = mysql_query("select *, date_format(creation, \"%Y-%m-%d %T\") as creat
 	or die("Query failed : " . mysql_error());
 while ($myrow = mysql_fetch_array($result)) 
 {
-	printf("<b>eventid:</b> %s ", $myrow["eventid"]);
+	if ( ($myrow["owner"] == null || $myrow["owner"] == "" ||
+		$myrow["owner"] == $_COOKIE["karchanadminname"]) )
+	{
+		printf("<b>eventid:</b> <A
+    	   HREF=\"/scripts/admin_commandseventsmethods.php?eventid=%s\">%s</A> ",
+	       $myrow["eventid"], $myrow["eventid"]);
+	}
+	else
+	{
+		printf("<b>eventid:</b> %s ",
+	       $myrow["eventid"]);
+	}
 	if ($myrow["room"] != null)
 	{
 		printf("<b>room:</b> <A
@@ -102,29 +290,21 @@ HREF=\"/scripts/admin_commandseventsmethods.php?methodname=%s\">%s</A> ", $myrow
 		$myrow["owner"] == $_COOKIE["karchanadminname"]) )
 	{
 ?>
-<FORM METHOD="GET" ACTION="/scripts/admin_rooms.php">
+<FORM METHOD="GET" ACTION="/scripts/admin_commandseventsmethods.php">
 <b>
-<INPUT TYPE="hidden" NAME="room" VALUE="<?php echo $myrow["eventid"] ?>">
+<INPUT TYPE="hidden" NAME="eventid" VALUE="<?php echo $myrow["eventid"] ?>">
 <TABLE>
-<TR><TD>west</TD><TD><INPUT TYPE="text" NAME="west" VALUE="<?php echo $myrow["west"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>east</TD><TD><INPUT TYPE="text" NAME="east" VALUE="<?php echo $myrow["east"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>north</TD><TD><INPUT TYPE="text" NAME="north" VALUE="<?php echo $myrow["north"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>south</TD><TD><INPUT TYPE="text" NAME="south" VALUE="<?php echo $myrow["south"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>up</TD><TD><INPUT TYPE="text" NAME="up" VALUE="<?php echo $myrow["up"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>down</TD><TD><INPUT TYPE="text" NAME="down" VALUE="<?php echo $myrow["down"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>contents</TD><TD><TEXTAREA NAME="contents" ROWS="14" COLS="85"><?php echo $myrow["contents"] ?></TEXTAREA></TD></TR>
-<TR><TD>area</TD><TD><SELECT NAME="method_name">
-<?php
-$arearesult = mysql_query("select name from mm_methods"
-	, $dbhandle)
-	or die("Query failed : " . mysql_error());
-while ($namemyrow = mysql_fetch_array($arearesult)) 
-{
-	printf("<option %s value=\"%s\">%s",
-		($namemyrow["name"] == $myrow["method_name"] ? "selected" : ""),
-		$namemyrow["name"], $namemyrow["name"]);
-}
-?>
+<TR><TD>character name:</TD><TD><INPUT TYPE="text" NAME="eventcharname" VALUE="<?php echo $myrow["name"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>room:</TD><TD><INPUT TYPE="text" NAME="eventroom" VALUE="<?php echo $myrow["room"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>month:</TD><TD><INPUT TYPE="text" NAME="eventmonth" VALUE="<?php echo $myrow["month"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>dayofmonth:</TD><TD><INPUT TYPE="text" NAME="eventdayofmonth" VALUE="<?php echo $myrow["dayofmonth"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>hour:</TD><TD><INPUT TYPE="text" NAME="eventhour" VALUE="<?php echo $myrow["hour"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>minute:</TD><TD><INPUT TYPE="text" NAME="eventminute" VALUE="<?php echo $myrow["minute"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>dayofweek:</TD><TD><INPUT TYPE="text" NAME="eventdayofweek" VALUE="<?php echo $myrow["dayofweek"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>method_name:</TD><TD><INPUT TYPE="text" NAME="eventmethodname" VALUE="<?php echo $myrow["method_name"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>callable</TD><TD><SELECT NAME="eventcallable" SIZE="2">
+<option value="1" <?php if ($myrow["callable"] == "1") {printf("selected");} ?>>yes
+<option value="0" <?php if ($myrow["callable"] == "0") {printf("selected");} ?>>no
 </SELECT></TD></TR>
 </TABLE>
 <INPUT TYPE="submit" VALUE="Change Event">
@@ -135,19 +315,159 @@ while ($namemyrow = mysql_fetch_array($arearesult))
 	}
 }
 ?>
+<FORM METHOD="GET" ACTION="/scripts/admin_commandseventsmethods.php">
+<b>
+Method name: <INPUT TYPE="text" NAME="addeventmethodname" VALUE="">
+<INPUT TYPE="submit" VALUE="Add Event">
+</b>
+</FORM>
+<P>
+
+<?php
+/* 
+------------------------------------------------------------------
+COMMANDS
+------------------------------------------------------------------
+*/
+?>
 
 <H2><A HREF="/karchan/admin/help/scripting2.html" target="_blank">
 <IMG SRC="/images/icons/9pt4a.gif" BORDER="0"></A>
 Commands</H2>
 
 <?php
+if ( isset($_REQUEST{"commandid"})
+	&& isset($_REQUEST{"commandroom"})
+    && isset($_REQUEST{"commandmethodname"})
+    && isset($_REQUEST{"commandcallable"}) )
+{
+	// check proper information formats
+	if ( !is_numeric($_REQUEST{"commandid"}) )
+	{
+		die("Expected commandid (".$_REQUEST{"commandid"}.") to be an integer, and it wasn't.");
+	}
+	// check that the command is the proper owner.
+    $result = mysql_query("select id from mm_commands where id = \"".
+        mysql_escape_string($_REQUEST{"commandid"}).  
+        "\" and (owner is null or owner = \"".   
+        mysql_escape_string($_COOKIE["karchanadminname"]).
+        "\")"
+        , $dbhandle)
+        or die("Query(1) failed : " . mysql_error());
+    if (mysql_num_rows($result) != 1)
+    {
+        die("You are not the owner of this command.");
+    }
+	// check to see that method exists
+	$result = mysql_query("select 1 from mm_methods where name = \"".
+		mysql_escape_string($_REQUEST{"commandmethodname"}).
+		"\""
+		, $dbhandle)
+		or die("Query failed : " . mysql_error());
+	if (mysql_num_rows($result) != 1)
+	{
+		die("Method does not exist.");
+	}
+    if ( ($_REQUEST{"commandcallable"} != "1") &&
+        ($_REQUEST{"commandcallable"} != "0") )
+    {
+        die("Callable should be either 0 or 1.");
+    }
+	// check to see that room exists
+	if (trim($_REQUEST{"commandroom"}) != "")
+	{
+		$result = mysql_query("select 1 from mm_rooms where id = \"".
+			mysql_escape_string($_REQUEST{"commandroom"}).
+			"\""
+			, $dbhandle)
+			or die("Query failed : " . mysql_error());
+		if (mysql_num_rows($result) != 1)
+		{
+			die("Room ".$_REQUEST{"commandroom"}." does not exist.");
+		}
+	}
+	// make that change
+	$query = "update mm_commands set command=\"".
+		mysql_escape_string($_REQUEST{"commandcommand"}).
+		"\", callable=\"".
+		mysql_escape_string($_REQUEST{"commandcallable"}).
+		"\", method_name=\"".
+		mysql_escape_string($_REQUEST{"commandmethodname"}).
+		"\", room=".
+		(trim($_REQUEST{"commandroom"}) != "" ?
+			"\"".mysql_escape_string($_REQUEST{"commandroom"})."\"":
+			"null").
+		", owner=\"".
+		mysql_escape_string($_COOKIE["karchanadminname"]).
+		"\" where id = \"".
+		mysql_escape_string($_REQUEST{"commandid"}).
+		"\"";
+    mysql_query($query
+        , $dbhandle)
+        or die("Query(8) failed : " . mysql_error());
+    writeLogLong($dbhandle, "Changed command ".$_REQUEST{"commandid"}.
+		".", $query);
+}
+if (isset($_REQUEST{"addcommandname"}) &&
+   isset($_REQUEST{"addcommandmethodname"}))
+{
+	// check to see that method exists
+	$result = mysql_query("select 1 from mm_methods where name = \"".
+		mysql_escape_string($_REQUEST{"addcommandmethodname"}).
+		"\""
+		, $dbhandle)
+		or die("Query failed : " . mysql_error());
+	if (mysql_num_rows($result) != 1)
+	{
+		die("Method does not exist.");
+	}
+	// create new id
+	$result = mysql_query("select max(id) as id from mm_commands"
+		, $dbhandle)
+		or die("Query failed : " . mysql_error());
+	$maxid = 0;
+	while ($myrow = mysql_fetch_array($result)) 
+	{
+		$maxid = $myrow["id"];
+	}
+	if ($maxid == 0)
+	{
+		die("Could not compute id for command.");
+	}
+	
+	// make that change
+	$query = "insert into mm_commands (id, command, method_name, owner) values(".
+		($maxid + 1).
+		", \"".
+		mysql_escape_string($_REQUEST{"addcommandname"}).
+		"\", \"".
+		mysql_escape_string($_REQUEST{"addcommandmethodname"}).
+		"\", \"".
+		mysql_escape_string($_COOKIE["karchanadminname"]).
+		"\")";
+    mysql_query($query
+        , $dbhandle)
+        or die("Query(8) failed : " . mysql_error());
+    writeLogLong($dbhandle, "Added command ".$_REQUEST{"addcommandname"}.".", $query);
+}
 $result = mysql_query("select *, date_format(creation, \"%Y-%m-%d %T\") as creation2 
 	from mm_commands"
 	, $dbhandle)
 	or die("Query failed : " . mysql_error());
 while ($myrow = mysql_fetch_array($result)) 
 {
-	printf("<b>id:</b> %s ", $myrow["id"]);
+	if ( ($myrow["owner"] == null || $myrow["owner"] == "" ||
+		$myrow["owner"] == $_COOKIE["karchanadminname"]) )
+	{
+		printf("<b>id:</b> <A
+    	   HREF=\"/scripts/admin_commandseventsmethods.php?commandid=%s\">%s</A> ",
+	       $myrow["id"], $myrow["id"]);
+	}
+	else
+	{
+		printf("<b>id:</b> %s ",
+	       $myrow["id"]);
+	}
 	printf("<b>callable:</b> %s ", ($myrow["callable"]=="1"?"yes":"no"));
 	printf("<b>command:</b> %s ", $myrow["command"]);
 	printf("<b>method_name:</b> <A
@@ -165,29 +485,16 @@ HREF=\"/scripts/admin_commandseventsmethods.php?methodname=%s\">%s</A> ", $myrow
 		$myrow["owner"] == $_COOKIE["karchanadminname"]) )
 	{
 ?>
-<FORM METHOD="GET" ACTION="/scripts/admin_rooms.php">
+<FORM METHOD="GET" ACTION="/scripts/admin_commandseventsmethods.php">
 <b>
-<INPUT TYPE="hidden" NAME="room" VALUE="<?php echo $myrow["commandid"] ?>">
+<INPUT TYPE="hidden" NAME="commandid" VALUE="<?php echo $myrow["id"] ?>">
 <TABLE>
-<TR><TD>west</TD><TD><INPUT TYPE="text" NAME="west" VALUE="<?php echo $myrow["west"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>east</TD><TD><INPUT TYPE="text" NAME="east" VALUE="<?php echo $myrow["east"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>north</TD><TD><INPUT TYPE="text" NAME="north" VALUE="<?php echo $myrow["north"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>south</TD><TD><INPUT TYPE="text" NAME="south" VALUE="<?php echo $myrow["south"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>up</TD><TD><INPUT TYPE="text" NAME="up" VALUE="<?php echo $myrow["up"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>down</TD><TD><INPUT TYPE="text" NAME="down" VALUE="<?php echo $myrow["down"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
-<TR><TD>contents</TD><TD><TEXTAREA NAME="contents" ROWS="14" COLS="85"><?php echo $myrow["contents"] ?></TEXTAREA></TD></TR>
-<TR><TD>area</TD><TD><SELECT NAME="method_name">
-<?php
-$arearesult = mysql_query("select name from mm_methods"
-	, $dbhandle)
-	or die("Query failed : " . mysql_error());
-while ($namemyrow = mysql_fetch_array($arearesult)) 
-{
-	printf("<option %s value=\"%s\">%s",
-		($namemyrow["name"] == $myrow["method_name"] ? "selected" : ""),
-		$namemyrow["name"], $namemyrow["name"]);
-}
-?>
+<TR><TD>command:</TD><TD><INPUT TYPE="text" NAME="commandcommand" VALUE="<?php echo $myrow["command"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>room:</TD><TD><INPUT TYPE="text" NAME="commandroom" VALUE="<?php echo $myrow["room"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>method_name:</TD><TD><INPUT TYPE="text" NAME="commandmethodname" VALUE="<?php echo $myrow["method_name"] ?>" SIZE="40" MAXLENGTH="40"></TD></TR>
+<TR><TD>callable</TD><TD><SELECT NAME="commandcallable" SIZE="2">
+<option value="1" <?php if ($myrow["callable"] == "1") {printf("selected");} ?>>yes
+<option value="0" <?php if ($myrow["callable"] == "0") {printf("selected");} ?>>no
 </SELECT></TD></TR>
 </TABLE>
 <INPUT TYPE="submit" VALUE="Change Command">
@@ -196,6 +503,23 @@ while ($namemyrow = mysql_fetch_array($arearesult))
 <?php
 	}
 }
+?>
+<FORM METHOD="GET" ACTION="/scripts/admin_commandseventsmethods.php">
+<b>
+Command name: <INPUT TYPE="text" NAME="addcommandname" VALUE="">
+<BR>
+Method name: <INPUT TYPE="text" NAME="addcommandmethodname" VALUE="">
+<INPUT TYPE="submit" VALUE="Add Command">
+</b>
+</FORM>
+<P>
+
+<?php
+/* 
+------------------------------------------------------------------
+METHODS
+------------------------------------------------------------------
+*/
 ?>
 
 <H2><A HREF="/karchan/admin/help/scripting3.html" target="_blank">
@@ -216,6 +540,19 @@ if (isset($_REQUEST{"methodname"}) && isset($_REQUEST{"src"}))
         , $dbhandle)
         or die("Query(8) failed : " . mysql_error());
     writeLogLong($dbhandle, "Changed method ".$_REQUEST{"methodname"}.".", $query);
+}
+if (isset($_REQUEST{"addmethodname"}))
+{
+	// make that change
+	$query = "insert into mm_methods (name, owner) values(\"".
+		mysql_escape_string($_REQUEST{"addmethodname"}).
+		"\", \"".
+		mysql_escape_string($_COOKIE["karchanadminname"]).
+		"\")";
+    mysql_query($query
+        , $dbhandle)
+        or die("Query(8) failed : " . mysql_error());
+    writeLogLong($dbhandle, "Added method ".$_REQUEST{"addmethodname"}.".", $query);
 }
 
 $result = mysql_query("select *, 
@@ -257,7 +594,15 @@ $myrow["src2"] ?></TEXTAREA></TD></TR>
 
 mysql_close($dbhandle);
 ?>
+
+<FORM METHOD="GET" ACTION="/scripts/admin_commandseventsmethods.php">
+<b>
+Method name: <INPUT TYPE="text" NAME="addmethodname" VALUE="">
+<INPUT TYPE="submit" VALUE="Add Method">
+</b>
+</FORM>
 <P>
+
 <a HREF="/karchan/admin/rooms.html">
 <img SRC="/images/gif/webpic/buttono.gif"  
 BORDER="0"></a><p>
