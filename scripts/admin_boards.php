839861e6a7846eacc351f39558a1db58edbda19a
karn <karn@1239004b-fd02-0410-9f86-fb9af6f58458>
2004-10-30 23:00:31 +0000

    /* added an overview lst of the available boards for administrators of the mud Also added a status.php script to replace the status.html file. This one retrieves all deputies from the database and lists them. This makes it possible to always have an updated list of all the deputies. Deputies that have expired are automatically excluded from this list. */

---
+++ scripts/admin_boards.php
@@ -0,0 +1,63 @@
+<?
+/*-------------------------------------------------------------------------
+cvsinfo: $Header$
+Maarten's Mud, WWW-based MUD using MYSQL
+Copyright (C) 1998  Maarten van Leunen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Maarten van Leunen
+Appelhof 27
+5345 KA Oss
+Nederland
+Europe
+maarten_l@yahoo.com
+-------------------------------------------------------------------------*/
+?>
+<HTML>
+<HEAD>
+<TITLE>
+Land of Karchan - Admin
+</TITLE>
+</HEAD>
+                                                                                                                      
+<BODY>
+<BODY BGCOLOR=#FFFFFF BACKGROUND="/images/gif/webpic/back4.gif">
+<H1>
+<IMG SRC="/images/gif/dragon.gif">Public/Private Boards</H1>
+<A HREF="/karchan/admin/help/boards.html" target="_blank">
+<IMG SRC="/images/icons/9pt4a.gif" BORDER="0"></A><P>
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/scripts/connect.php"; 
+include $_SERVER['DOCUMENT_ROOT']."/scripts/admin_authorize.php";
+
+$result = mysql_query("select *, date_format(creation, \"%Y-%m-%d %T\") as
+        creation2 from mm_boards"
+	, $dbhandle)
+	or die("Query failed : " . mysql_error());
+while ($myrow = mysql_fetch_array($result)) 
+{
+	printf("<b>id:</b> %s ", $myrow["id"]);
+	printf("<b>name:</b> %s ", $myrow["name"]);
+	printf("<b>owner:</b> %s ", $myrow["owner"]);
+	printf("<b>creation:</b> %s<BR>", $myrow["creation2"]);
+	printf("<b>description:</b> %s<BR>", $myrow["description"]);
+}
+
+mysql_close($dbhandle);
+?>
+
+</BODY>
+</HTML>
---
+++ scripts/status.php
@@ -0,0 +1,95 @@
+<?
+/*-------------------------------------------------------------------------
+cvsinfo: $Header$
+Maarten's Mud, WWW-based MUD using MYSQL
+Copyright (C) 1998  Maarten van Leunen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Maarten van Leunen
+Appelhof 27
+5345 KA Oss
+Nederland
+Europe
+maarten_l@yahoo.com
+-------------------------------------------------------------------------*/
+?>
+<HTML>
+<HEAD>
+<TITLE>
+Land of Karchan - Status
+</TITLE>
+</HEAD>
+<BODY>
+<BODY BGCOLOR=#FFFFFF BACKGROUND="/images/gif/webpic/back4.gif">
+<H1><IMG SRC="/images/gif/dragon.gif">
+Status of Land of Karchan</H1>
+
+There is a hierarchie in the game and some have more powers and some less.
+The reason for this that some people have to be accountable to others, and
+everybody is accountable to the all mighty Implementor in the sky. The
+hierachy is as follows:<P>
+
+<H3>Implementors</H3>
+<UL>
+<LI><A HREF="mailto:karn@karchan.org">Karn</A>, Ruler of Karchan, Keeper of the Key to the Room of Lost Souls
+</UL>
+<H3><A HREF="mailto:deputy@karchan.org">Administrators</A></H3>
+<UL>
+
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/scripts/connect.php"; 
+
+// show results
+$result = mysql_query("select concat(\"<LI><A HREF=\\\"mailto:\", 
+mm_admin.name, \"@karchan.org\\\">\", mm_admin.name, \"</A>, \", title)
+	as name from mm_admin, mm_usertable 
+where mm_admin.name = mm_usertable.name and mm_admin.validuntil > now()"
+	, $dbhandle)
+	or die("Query failed : " . mysql_error());
+while ($myrow = mysql_fetch_array($result)) 
+{
+	printf($myrow["name"]);
+}
+mysql_close($dbhandle);
+?>
+
+<?php
+if ((isset($_COOKIE{"karchanpassword"})) &&
+    (isset($_COOKIE["karchanname"])))
+{   
+?>
+Add Bug Report:<P>
+<FORM METHOD="GET" ACTION="/scripts/bugs.php">
+Title: <INPUT TYPE="text" NAME="title" VALUE="" SIZE="60"><BR>
+Description:<BR>
+<TEXTAREA NAME="description" VALUE="" ROWS="30" COLS="85"></TEXTAREA><P>
+<INPUT TYPE="submit" VALUE="Submit">
+<INPUT TYPE="reset" VALUE="Clear"><P>
+</FORM>
+<?php
+} 
+?>
+
+</UL>
+<P>
+
+<A HREF="/karchan/help/helpindex.html">
+<IMG SRC="/images/gif/webpic/buttono.gif"  
+BORDER="0"></A><P>
+
+</BODY>
+</HTML>
+
