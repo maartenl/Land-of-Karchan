2eb70b331fceeb329809ab691e337fd0f9d99027
karchan <karchan@1239004b-fd02-0410-9f86-fb9af6f58458>
2005-03-17 13:00:05 +0000

    /* added the possibility to add and remove items to characters/rooms/containers. This should help a lot. */

--- scripts/admin_items.php
+++ scripts/admin_items.php
@@ -42,6 +42,43 @@ Item <?php echo $_REQUEST{"item"} ?></H1>
 <?php
 include $_SERVER['DOCUMENT_ROOT']."/scripts/connect.php"; 
 include $_SERVER['DOCUMENT_ROOT']."/scripts/admin_authorize.php";
+/* the following constraints need to be checked before any kind of update is
+to take place:
+
+adding item to room:
+- check itemid is integer/numeric
+- check if roomid is integer/numeric
+- check if owner is null or correct
+- check that room exists
+deleting item from room:
+- check itemid is integer/numeric
+- check if owner is null or correct
+
+adding item to char:
+- check itemid is integer/numeric
+- check if owner is null or correct
+- check that character exists
+deleting item from char:
+- check itemid is integer/numeric
+- check if owner is null or correct
+
+adding item to container:
+- check itemid is integer/numeric
+- check containerid is integer/numeric
+- check if owner is null or correct
+- check that container exists AND make sure that the item is a container!!
+- check that the container referenced is not the item itself.
+deleting item from container:
+- check itemid is integer/numeric
+- check if owner is null or correct
+*/
+$owner = false;
+$claimed = false; // is the item allocated to a character/room or container?
+
+if (!isset($_REQUEST{"item"}))
+{
+	die("Form information missing.");
+}
 $result = mysql_query("select mm_itemtable.id, mm_itemtable.owner, 
 mm_itemtable.itemid,
 date_format(mm_itemtable.creation, \"%Y-%m-%d %T\") as creation2, 
@@ -67,6 +104,181 @@ while ($myrow = mysql_fetch_array($result))
 
 }
 
+if (isset($_REQUEST{"removeitemfromroom"}))
+{
+	// check numeric stuff
+	if (!is_numeric($_REQUEST{"removeitemfromroom"}))
+	{
+		die("Expected field to be an integer, and it wasn't.");
+	}
+	// check if owner is correct
+	if (!$owner)
+	{
+		die("You are not the owner of this item instance.");
+	}
+	$query = "delete from mm_roomitemtable where id = ".
+	mysql_escape_string($_REQUEST{"removeitemfromroom"});
+	mysql_query($query, $dbhandle)
+	or die("Query (".$query.") failed : " . mysql_error());
+	if (mysql_affected_rows() < 1)
+	{
+		die("Item does not exist in a room.");
+	}
+	writeLogLong($dbhandle, "Removed item ".$_REQUEST{"removeitemfromroom"}." from room(s).", $query);
+}
+if (isset($_REQUEST{"removeitemfromchar"}))
+{
+	// check numeric stuff
+	if (!is_numeric($_REQUEST{"removeitemfromchar"}))
+	{
+		die("Expected field to be an integer, and it wasn't.");
+	}
+	// check if owner is correct
+	if (!$owner)
+	{
+		die("You are not the owner of this item instance.");
+	}
+	$query = "delete from mm_charitemtable where id = ".
+	mysql_escape_string($_REQUEST{"removeitemfromchar"});
+	mysql_query($query, $dbhandle)
+	or die("Query (".$query.") failed : " . mysql_error());
+	if (mysql_affected_rows() < 1)
+	{
+		die("Item does not belong to a character.");
+	}
+	writeLogLong($dbhandle, "Removed item ".$_REQUEST{"removeitemfromchar"}." from character(s).", $query);
+}
+if (isset($_REQUEST{"removeitemfromitem"}))
+{
+	// check numeric stuff
+	if (!is_numeric($_REQUEST{"removeitemfromitem"}))
+	{
+		die("Expected field to be an integer, and it wasn't.");
+	}
+	// check if owner is correct
+	if (!$owner)
+	{
+		die("You are not the owner of this item instance.");
+	}
+	$query = "delete from mm_itemitemtable where id = ".
+	mysql_escape_string($_REQUEST{"removeitemfromitem"});
+	mysql_query($query, $dbhandle)
+	or die("Query (".$query.") failed : " . mysql_error());
+	if (mysql_affected_rows() < 1)
+	{
+		die("Item is not stored in a container.");
+	}
+	writeLogLong($dbhandle, "Removed item ".$_REQUEST{"removeitemfromitem"}." from container(s).", $query);
+}
+if (isset($_REQUEST{"additemtoroom"}))
+{
+	// check numeric stuff
+	if (!is_numeric($_REQUEST{"additemtoroom"}))
+	{
+		die("Expected field to be an integer, and it wasn't.");
+	}
+	if (!is_numeric($_REQUEST{"additemtoroom_roomid"}))
+	{
+		die("Expected field to be an integer, and it wasn't.");
+	}
+	// check if owner is correct
+	if (!$owner)
+	{
+		die("You are not the owner of this item instance.");
+	}
+	// check that room exists
+	$result = mysql_query("select id from mm_rooms where id = ".
+	mysql_escape_string($_REQUEST{"additemtoroom_roomid"})
+	, $dbhandle)
+	or die("Query(2) failed : " . mysql_error());
+	if (mysql_num_rows($result) != 1)
+	{
+		die("Room does not exist.");
+	}
+	
+	$query = "insert into mm_roomitemtable (id, room) values(".
+	mysql_escape_string($_REQUEST{"additemtoroom"}).
+	", ".
+	mysql_escape_string($_REQUEST{"additemtoroom_roomid"}).
+	")";
+	mysql_query($query, $dbhandle)
+	or die("Query (".$query.") failed : " . mysql_error());
+	writeLogLong($dbhandle, "Added item ".$_REQUEST{"additemtoroom"}." to room ".$_REQUEST{"additemtoroom_roomid"}.".", $query);
+}
+if (isset($_REQUEST{"additemtochar"}))
+{
+	// check numeric stuff
+	if (!is_numeric($_REQUEST{"additemtochar"}))
+	{
+		die("Expected field to be an integer, and it wasn't.");
+	}
+	// check if owner is correct
+	if (!$owner)
+	{
+		die("You are not the owner of this item instance.");
+	}
+	// check that character exists
+	$result = mysql_query("select name from mm_usertable where name = \"".
+	mysql_escape_string($_REQUEST{"additemtochar_charname"}).
+	"\""
+	, $dbhandle)
+	or die("Query(2) failed : " . mysql_error());
+	if (mysql_num_rows($result) != 1)
+	{
+		die("Character does not exist.");
+	}
+
+	$query = "insert into mm_charitemtable (id, belongsto) values(".
+	mysql_escape_string($_REQUEST{"additemtochar"}).
+	", \"".
+	mysql_escape_string($_REQUEST{"additemtochar_charname"}).
+	"\")";
+	mysql_query($query, $dbhandle)
+	or die("Query (".$query.") failed : " . mysql_error());
+	writeLogLong($dbhandle, "Added item ".$_REQUEST{"additemtochar"}." to character ".$_REQUEST{"additemtochar_charname"}.".", $query);
+}
+if (isset($_REQUEST{"additemtocontainer"}))
+{
+	// check numeric stuff
+	if (!is_numeric($_REQUEST{"additemtocontainer"}))
+	{
+		die("Expected field to be an integer, and it wasn't.");
+	}
+	if (!is_numeric($_REQUEST{"additemtocontainer_containerid"}))
+	{
+		die("Expected field to be an integer, and it wasn't.");
+	}
+	// check if owner is correct
+	if (!$owner)
+	{
+		die("You are not the owner of this item instance.");
+	}
+	// check that not item is same as container
+	if ($_REQUEST{"additemtocontainer_containerid"} == $_REQUEST{"additemtocontainer"})
+	{
+		die("You cannot put an item into itself.");
+	}
+	// check that container exists and is in fact a container
+	$result = mysql_query("select mm_itemtable.itemid from mm_itemtable, mm_items ".
+	"where mm_itemtable.itemid = mm_items.id and mm_items.container = 1 and mm_itemtable.id = ".
+	mysql_escape_string($_REQUEST{"additemtocontainer_containerid"})
+	, $dbhandle)
+	or die("Query(2) failed : " . mysql_error());
+	if (mysql_num_rows($result) != 1)
+	{
+		die("Container does not exist or is not a container.");
+	}
+
+	$query = "insert into mm_itemitemtable (id, containerid) values(".
+	mysql_escape_string($_REQUEST{"additemtocontainer"}).
+	", ".
+	mysql_escape_string($_REQUEST{"additemtocontainer_containerid"}).
+	")";
+	mysql_query($query, $dbhandle)
+	or die("Query (".$query.") failed : " . mysql_error());
+	writeLogLong($dbhandle, "Added item ".$_REQUEST{"additemtocontainer"}." to container ".$_REQUEST{"additemtocontainer_containerid"}.".", $query);
+}
+
 $result = mysql_query("select containerid ".
 	" from mm_itemitemtable".
 	" where mm_itemitemtable.id =
@@ -75,7 +287,15 @@ $result = mysql_query("select containerid ".
 	or die("Query failed : " . mysql_error());
 while ($myrow = mysql_fetch_array($result)) 
 {
-	printf("<b>Item contained in item:</b> <A HREF=\"/scripts/admin_items.php?item=%s\">%s</A><BR> ", $myrow[0], $myrow[0]);
+?>
+<FORM METHOD="GET" ACTION="/scripts/admin_items.php">
+<INPUT TYPE="hidden" NAME="removeitemfromitem" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="hidden" NAME="item" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="submit" VALUE="Remove From Container">
+<b>Item contained in item:</b> <A HREF="/scripts/admin_items.php?item=<?php echo $myrow[0] ?>"><?php echo $myrow[0] ?></A><BR>
+</FORM>
+<?php
+	$claimed = true;
 }
 $result = mysql_query("select room ".
 	" from mm_roomitemtable".
@@ -85,7 +305,15 @@ $result = mysql_query("select room ".
 	or die("Query failed : " . mysql_error());
 while ($myrow = mysql_fetch_array($result)) 
 {
-	printf("<b>Item contained in room:</b> <A HREF=\"/scripts/admin_rooms.php?room=%s\">%s</A><BR> ", $myrow[0], $myrow[0]);
+?>
+<FORM METHOD="GET" ACTION="/scripts/admin_items.php">
+<INPUT TYPE="hidden" NAME="removeitemfromroom" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="hidden" NAME="item" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="submit" VALUE="Remove From Room">
+<b>Item contained in room:</b> <A HREF="/scripts/admin_rooms.php?room=<?php echo $myrow[0] ?>"><?php echo $myrow[0] ?></A><BR>
+</FORM>
+<?php
+	$claimed = true;
 }
 $result = mysql_query("select belongsto ".
 	" from mm_charitemtable".
@@ -95,7 +323,15 @@ $result = mysql_query("select belongsto ".
 	or die("Query failed : " . mysql_error());
 while ($myrow = mysql_fetch_array($result)) 
 {
-	printf("<b>Item belongsto character:</b> <A HREF=\"/scripts/admin_chars.php?char=%s\">%s</A><BR> ", $myrow[0], $myrow[0]);
+?>
+<FORM METHOD="GET" ACTION="/scripts/admin_items.php">
+<INPUT TYPE="hidden" NAME="removeitemfromchar" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="hidden" NAME="item" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="submit" VALUE="Remove From Character">
+<b>Item belongsto character:</b> <A HREF="/scripts/admin_chars.php?char=<?php echo $myrow[0] ?>"><?php echo $myrow[0] ?></A><BR>
+</FORM>
+<?php
+	$claimed = true;
 }
 
 $result = mysql_query("select mm_itemitemtable.id, mm_items.id, ".
@@ -111,6 +347,30 @@ while ($myrow = mysql_fetch_array($result))
 	printf("<b>id:</b> <A HREF=\"/scripts/admin_items.php?item=%s\">%s</A> ", $myrow[0], $myrow[0]);
 	printf("<b>itemid:</b> %s ", $myrow[1]);
 	printf("<b>description:</b> %s %s %s %s<BR>", $myrow[2], $myrow[3], $myrow[4], $myrow[5]);
+}
+
+if (($owner) && (!$claimed))
+{
+?>
+<FORM METHOD="GET" ACTION="/scripts/admin_items.php">
+<INPUT TYPE="hidden" NAME="additemtoroom" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="hidden" NAME="item" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="submit" VALUE="Add To Room">
+Roomid: <INPUT TYPE="text" NAME="additemtoroom_roomid" VALUE="" SIZE="40" MAXLENGTH="40">
+</FORM>
+<FORM METHOD="GET" ACTION="/scripts/admin_items.php">
+<INPUT TYPE="hidden" NAME="additemtochar" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="hidden" NAME="item" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="submit" VALUE="Add To Character">
+Charactername: <INPUT TYPE="text" NAME="additemtochar_charname" VALUE="" SIZE="40" MAXLENGTH="40">
+</FORM>
+<FORM METHOD="GET" ACTION="/scripts/admin_items.php">
+<INPUT TYPE="hidden" NAME="additemtocontainer" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="hidden" NAME="item" VALUE="<?php echo $_REQUEST{"item"} ?>">
+<INPUT TYPE="submit" VALUE="Add To Container">
+Containerid: <INPUT TYPE="text" NAME="additemtocontainer_containerid" VALUE="" SIZE="40" MAXLENGTH="40">
+</FORM>
+<?php
 }
 
 printf("<H2><A HREF=\"/karchan/admin/help/attributes.html\" target=\"_blank\">
@@ -133,7 +393,7 @@ if (isset($_REQUEST{"item"}) &&
 		.mysql_escape_string($_REQUEST{"item"}).
 		"\")";
 	mysql_query($query
-		, $dbhandle)  
+		, $dbhandle)
 		or die("Query(8) failed : " . mysql_error());
 	writeLogLong($dbhandle, "Added attribute to ".$_REQUEST{"char"}.".", $query);
 }
@@ -163,10 +423,10 @@ if ($owner)
 <OPTION VALUE="string" selected >string
 <OPTION VALUE="integer">integer
 <OPTION VALUE="boolean">boolean
-</SELECT></TD></TR>   
+</SELECT></TD></TR> 
 </TABLE>
 <INPUT TYPE="submit" VALUE="Add Attribute">
-</b>   
+</b> 
 </FORM>
 <?php
 	}
