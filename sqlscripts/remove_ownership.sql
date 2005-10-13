78d645094e80da99adf7ed6bda375b6762e4ac66
maartenl <maartenl@1239004b-fd02-0410-9f86-fb9af6f58458>
2005-10-13 07:33:16 +0000

    /* a new directory containig fresh sql scripts */

---
+++ sqlscripts/remove_ownership.sql
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+. ./mysql_constants
+
+${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL_DB} <<END_OF_DATA
+#
+# This has to be done before the deputy/administrator is
+# removed from the administrator table.
+
+update mm_methods set owner=null where owner="Argo";
+update mm_commands set owner=null where owner="Argo";
+update mm_events set owner=null where owner="Argo";
+update mm_items set owner=null where owner="Argo";
+update mm_rooms set owner=null where owner="Argo";
+update mm_boards set owner=null where owner="Argo";
+update mm_usertable set owner=null where owner="Argo";
+update mm_area set owner=null where owner="Argo";
+
+END_OF_DATA
+
