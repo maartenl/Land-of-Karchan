36588445b8631e5f39020b8ccf3ae8fffd630b30
maartenl <maartenl@1239004b-fd02-0410-9f86-fb9af6f58458>
2005-10-27 20:18:45 +0000

    /* added a possibility for changing the decription of the boards. Temporarily, I plan on implementing it in the admin scripts, like I should. */

---
+++ sqlscripts/set_boarddescription.sql
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+. ./mysql_constants
+
+${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL_DB} <<END_OF_DATA
+#
+# update the description of a specific board.
+#
+
+update mm_boards set description='<b>Logonmessage</b>
+<br>
+<img SRC="/images/gif/letters/w.gif"
+ALT="W" ALIGN=left>
+elcome to the Land of Karchan MUD, a land filled with mystery
+and enchantment, where weapons, magic, intelligence, and common
+sense play key roles in the realm. Where love and war can be one
+and the same. Where elves coexist peacefully with the humans, and
+make war with the dwarves. Where the sun rises, and the moon falls.
+Where one can change into a hero with a single swipe of his
+sword.<p>
+<A HREF="/scripts/bugs.php" target="_blank">
+<IMG ALIGN=right SRC="/images/gif/webpic/button.bugs.gif"
+BORDER="0"></A>
+Use the button you see on the left to register anything you 
+find wrong with the
+mud <I>or</I> anything you would like added.
+<P>' 
+where id=2;
+
+END_OF_DATA
+
