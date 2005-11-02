794b401235b019f7bd4daf890f158baf005081dc
maartenl <maartenl@1239004b-fd02-0410-9f86-fb9af6f58458>
2005-11-02 20:44:31 +0000

    /* changed old_password to sha1() ethod Due to mysql 4.1 */

--- scripts/admin_resetcharpasswd.php
+++ scripts/admin_resetcharpasswd.php
@@ -53,7 +53,7 @@ if (strlen($password) < 5)
 	error_message("Password should be at least 5 characters long.");
 }
 // make that change.
-$query = "update mm_usertable set password = old_password(\"".
+$query = "update mm_usertable set password = sha1(\"".
 	$password.
 	"\"), owner=\"".
 	quote_smart($_COOKIE["karchanadminname"]).
