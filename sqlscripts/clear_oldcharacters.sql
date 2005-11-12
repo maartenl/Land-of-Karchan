e0b9317c69ae3450d6e2957355f1732c84b86bca
maartenl <maartenl@1239004b-fd02-0410-9f86-fb9af6f58458>
2005-11-12 11:07:12 +0000

    /* did some stuff, needs to be finished bfore we cqn begin cleaning up old users. */

--- sqlscripts/clear_oldcharacters.sql
+++ sqlscripts/clear_oldcharacters.sql
@@ -7,11 +7,19 @@ ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL
 # totally delete a specific user from the database.
 #
 
-# insert into mm_log (name, message) ("$1", "deleted, not logged on since $2");
 
-select name, "$2" as lastlogin
-from mm_usertable
+delete from mm_charattributes
+where charname = "$1";
+
+delete from mm_usertable
 where name = "$1";
+
+delete from mm_itemtable, mm_charitemtable
+where mm_charitemtable.belongsto = "$1";
+
+/* error! What do we do with the damn items in a container? */
+
+insert into mm_log (name, message) values("$1", "deleted during cleanup, not logged on since $2.");
 
 END_OF_DATA
 
