1f989784b0cfb1456f45c41f29df4d3df338caf0
maartenl <maartenl@1239004b-fd02-0410-9f86-fb9af6f58458>
2005-11-12 12:01:54 +0000

    /* implemented something I am satisfied with. It uses temporary tables but that seems to be impossible to avoid. The script deletes a character from the database, and I mean really delete: it removes: - items - mudmail - attributes - user itself */

--- sqlscripts/clear_oldcharacters.sql
+++ sqlscripts/clear_oldcharacters.sql
@@ -8,18 +8,49 @@ ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL
 #
 
 
+# delete all items in containers that belong to the user
+create temporary table tmptable
+select mm_itemitemtable.id 
+from mm_charitemtable, mm_itemitemtable
+where mm_charitemtable.belongsto = "$1" and
+mm_itemitemtable.containerid = mm_charitemtable.id;
+
+delete from mm_itemitemtable
+where id in (select id from tmptable);
+delete from mm_itemtable
+where id in (select id from tmptable);
+
+drop table tmptable;
+# end of 'delete all items in containers that belong to the user'
+
+# delete all items that belong to the user
+create temporary table tmptable
+select mm_charitemtable.id 
+from mm_charitemtable
+where mm_charitemtable.belongsto = "$1";
+
+delete from mm_charitemtable
+where id in (select id from tmptable);
+delete from mm_itemtable
+where id in (select id from tmptable);
+
+drop table tmptable;
+# end of 'delete all items that belong to the user
+
+# delete all mudmail from and to the user
+delete from mm_mailtable where
+name = "$1" or toname = "$1";
+# end of 'delete all mudmail from and to the user
+
 delete from mm_charattributes
 where charname = "$1";
 
 delete from mm_usertable
 where name = "$1";
 
-delete from mm_itemtable, mm_charitemtable
-where mm_charitemtable.belongsto = "$1";
-
 /* error! What do we do with the damn items in a container? */
 
-insert into mm_log (name, message) values("$1", "deleted during cleanup, not logged on since $2.");
+insert into mm_log (name, message) values("$1", "deleted during cleanup, not logged on since $2 $3.");
 
 END_OF_DATA
 
--- sqlscripts/wrapper.sh
+++ sqlscripts/wrapper.sh
@@ -5,4 +5,4 @@
 # used for wrapping a select of all users not logged on for more than 6 months
 # and deleting them
 
-./select_oldcharacters.sql | xargs -n 2 ./clear_oldcharacters.sql
+./select_oldcharacters.sql | xargs -n 3 ./clear_oldcharacters.sql
