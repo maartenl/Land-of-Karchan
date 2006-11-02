96cd521ba91c39c5dc7a97c6cb7a6ec244aadd35
maartenl <maartenl@1239004b-fd02-0410-9f86-fb9af6f58458>
2006-11-02 10:02:58 +0000

    /* added some more tables that were causing problems. Replaced Argo with Crissy */

--- sqlscripts/remove_ownership.sql
+++ sqlscripts/remove_ownership.sql
@@ -8,19 +8,21 @@ ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL
 # removed from the administrator table.
 #
 
-update mm_methods set owner=null where owner="Argo";
-update mm_commands set owner=null where owner="Argo";
-update mm_events set owner=null where owner="Argo";
-update mm_items set owner=null where owner="Argo";
-update mm_rooms set owner=null where owner="Argo";
-update mm_boards set owner=null where owner="Argo";
-update mm_usertable set owner=null where owner="Argo";
-update mm_area set owner=null where owner="Argo";
+update polls set owner=null where owner="Crissy";
+update mm_guilds set owner=null where owner="Crissy";
+update mm_methods set owner=null where owner="Crissy";
+update mm_commands set owner=null where owner="Crissy";
+update mm_events set owner=null where owner="Crissy";
+update mm_items set owner=null where owner="Crissy";
+update mm_rooms set owner=null where owner="Crissy";
+update mm_boards set owner=null where owner="Crissy";
+update mm_usertable set owner=null where owner="Crissy";
+update mm_area set owner=null where owner="Crissy";
 
 #
 # remove the administrator from the administrator table
 #
-delete from mm_admin where name="Argo";
+delete from mm_admin where name="Crissy";
 
 END_OF_DATA
 
