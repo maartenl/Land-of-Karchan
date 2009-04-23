f1e0633d0b367aaac80b547ca07e72583cddaca2
maartenl <maartenl@1239004b-fd02-0410-9f86-fb9af6f58458>
2009-04-23 09:55:55 +0000

    /* separated the two sql scripts by means of an intermediary file. Do not wish to keep the connection open. */

--- sqlscripts/wrapper.sh
+++ sqlscripts/wrapper.sh
@@ -5,4 +5,5 @@
 # used for wrapping a select of all users not logged on for more than 6 months
 # and deleting them
 
-./select_oldcharacters.sql | xargs -n 3 ./clear_oldcharacters.sql
+./select_oldcharacters.sql > list.txt
+cat list.txt | xargs -n 3 ./clear_oldcharacters.sql
