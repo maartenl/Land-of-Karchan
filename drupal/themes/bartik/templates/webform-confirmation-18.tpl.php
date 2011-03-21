42d1f33d5fbf7f437046aeb6d515a4c7bf1e028f
maartenl <maartenl@1239004b-fd02-0410-9f86-fb9af6f58458>
2011-03-21 15:44:31 +0000

    /* changes to the logo. changes to the css style sheet. added my own font added templates for the following specifics: - who page - karchan game page - logging in form - new character form */

---
+++ drupal/themes/bartik/templates/webform-confirmation-18.tpl.php
@@ -0,0 +1,31 @@
+<?php
+// $Id: webform-confirmation.tpl.php,v 1.2 2010/02/11 22:05:27 quicksketch Exp $
+
+/**
+ * @file
+ * Customize confirmation screen after successful submission.
+ *
+ * This file may be renamed "webform-confirmation-[nid].tpl.php" to target a
+ * specific webform e-mail on your site. Or you can leave it
+ * "webform-confirmation.tpl.php" to affect all webform confirmations on your
+ * site.
+ *
+ * So now this file is specific to node 18, which is mudnewchar.
+ *
+ * Available variables:
+ * - $node: The node object for this webform.
+ * - $confirmation_message: The confirmation message input by the webform author.
+ * - $sid: The unique submission ID of this submission.
+ */
+?>
+
+<div class="webform-confirmation">
+  <?php if ($confirmation_message): ?>
+    <?php print $confirmation_message ?>
+  <?php endif; ?>
+    <p><?php print $_SESSION["karchan_errormsg"]; ?></p>
+</div>
+
+<div class="links">
+  <a href="<?php print url('node/'. $node->nid) ?>"><?php print t('Go back to the form') ?></a>
+</div>
