d9f3de5ea4378bd6c8b862e2cd302ab0b194b90f
Maarten van Leunen <maarten_l@yahoo.com>
2012-05-27 12:05:21 +0200

    - bug in teh script, would terminate all bots

--- sqlscripts/set_admin_rights.sql
+++ sqlscripts/set_admin_rights.sql
@@ -7,13 +7,19 @@ ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL
 # This has to be done periodically to set the proper admin rights.
 #
 
-update mm_usertable set god = 0;
 update mm_usertable set god = 1 
 where exists (
 	select * 
 	from mm_admin 
 	where mm_admin.name = mm_usertable.name 
 	and validuntil > now());
+
+update mm_usertable set god = 0 
+where exists (
+	select * 
+	from mm_admin 
+	where mm_admin.name = mm_usertable.name 
+	and validuntil < now());
 
 END_OF_DATA
 
