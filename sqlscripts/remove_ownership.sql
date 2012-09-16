63f124182203deee347a969fe833ee08e1349a93
Maarten van Leunen <maarten_l@yahoo.com>
2012-09-16 11:49:37 +0200

    more dynamic scriptshere.

--- sqlscripts/remove_ownership.sql
+++ sqlscripts/remove_ownership.sql
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+echo name of script is $0
+echo first argument is $1
+echo second argument is $2
+echo all arguments is $@
+echo number of arguments is $#
+
+if [ "$#" -ne 1 ]
+then
+    echo "need an administrator name"
+    exit 0
+fi
+                  
 . ./mysql_constants
 
 ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL_DB} <<END_OF_DATA
@@ -8,21 +20,21 @@ ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL
 # removed from the administrator table.
 #
 
-update polls set owner=null where owner="Crissy";
-update mm_guilds set owner=null where owner="Crissy";
-update mm_methods set owner=null where owner="Crissy";
-update mm_commands set owner=null where owner="Crissy";
-update mm_events set owner=null where owner="Crissy";
-update mm_items set owner=null where owner="Crissy";
-update mm_rooms set owner=null where owner="Crissy";
-update mm_boards set owner=null where owner="Crissy";
-update mm_usertable set owner=null where owner="Crissy";
-update mm_area set owner=null where owner="Crissy";
+update polls set owner=null where owner="$1";
+update mm_guilds set owner=null where owner="$1";
+update mm_methods set owner=null where owner="$1";
+update mm_commands set owner=null where owner="$1";
+update mm_events set owner=null where owner="$1";
+update mm_items set owner=null where owner="$1";
+update mm_rooms set owner=null where owner="$1";
+update mm_boards set owner=null where owner="$1";
+update mm_usertable set owner=null where owner="$1";
+update mm_area set owner=null where owner="$1";
 
 #
 # remove the administrator from the administrator table
 #
-delete from mm_admin where name="Crissy";
+delete from mm_admin where name="$1";
 
 END_OF_DATA
 
