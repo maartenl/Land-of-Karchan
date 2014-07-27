12adc5ec5ee3e9e710f5b437798f6ac43130381f
Maarten van Leunen <maarten_l@yahoo.com>
2014-07-27 12:57:08 +0200

    /* mutation script for the new database has an entry for creating the new mm_commandlog table (as it is not backed up.) */

--- sqlscripts/mutate_database_for_karchangame.sql
+++ sqlscripts/mutate_database_for_karchangame.sql
@@ -305,6 +305,21 @@ ALTER TABLE `mmud`.`mm_events`
 , ADD INDEX `fk_mm_events_1_idx` (`name` ASC) 
 , ADD INDEX `fk_mm_events_2_idx` (`room` ASC) ;
 
+#
+# this table is always forgotten in backups, as it tends to fill up quickly and is just
+# logging.
+# NOTE: FOr some reason this script doesn't work, but copy-pasting it in a mysql session does. No clue why.
+
+DROP TABLE IF EXISTS `mm_commandlog`;
+
+CREATE TABLE `mm_commandlog` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(20) NOT NULL,
+  `command` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7915438 DEFAULT CHARSET=latin1;
+                   
 END_OF_DATA
 
 echo "Adding trigger to the usertable..."
@@ -313,5 +328,4 @@ echo "Adding trigger to the usertable..."
 cd triggers
 ./mm_usertable_insert_trigger.sql
 cd ..
-
 
