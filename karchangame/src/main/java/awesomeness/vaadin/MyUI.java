1a48eb188db6cb3a4a620fd7697e28ba755a2270
Maarten van Leunen <maarten_l@yahoo.com>
2015-10-06 07:03:14 +0200

    added the first four administration parts.

--- karchangame/src/main/java/awesomeness/vaadin/MyUI.java
+++ karchangame/src/main/java/awesomeness/vaadin/MyUI.java
@@ -1,7 +1,5 @@
 package awesomeness.vaadin;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
@@ -9,36 +7,82 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import javax.servlet.annotation.WebServlet;
 
 /**
  *
  */
 @Theme("mytheme")
 @Widgetset("awesomeness.vaadin.MyAppWidgetset")
-public class MyUI extends UI {
+public class MyUI extends UI
+{
 
     @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
+    protected void init(VaadinRequest vaadinRequest)
+    {
+        Panel menuPanel = new Panel("Menu");
+        Panel contentPanel = new Panel("Content");
 
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
+        HorizontalSplitPanel splitPanel = new HorizontalSplitPanel(menuPanel, contentPanel);
+        setContent(splitPanel);
+
+        final FormLayout menuLayout = new FormLayout();
+        menuPanel.setContent(menuLayout);
+
+        Button worldattributes = new Button("World attributes");
+        worldattributes.addClickListener(new Button.ClickListener()
+        {
             @Override
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
+            public void buttonClick(ClickEvent event)
+            {
+                menuLayout.addComponent(new Label("Thank you for clicking"));
             }
         });
-        layout.addComponent(button);
+        menuLayout.addComponent(worldattributes);
+
+        Button scripts = new Button("Scripts");
+        scripts.addClickListener(new Button.ClickListener()
+        {
+            @Override
+            public void buttonClick(ClickEvent event)
+            {
+                menuLayout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        menuLayout.addComponent(scripts);
+
+        Button areas = new Button("Areas");
+        areas.addClickListener(new Button.ClickListener()
+        {
+            @Override
+            public void buttonClick(ClickEvent event)
+            {
+                menuLayout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        menuLayout.addComponent(areas);
+
+        Button events = new Button("Events");
+        events.addClickListener(new Button.ClickListener()
+        {
+            @Override
+            public void buttonClick(ClickEvent event)
+            {
+                menuLayout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        menuLayout.addComponent(events);
 
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
+    public static class MyUIServlet extends VaadinServlet
+    {
     }
 }
