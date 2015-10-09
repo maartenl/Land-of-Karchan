01ca05d1f18e1076457889ef71df0dec448c454b
Maarten van Leunen <maarten_l@yahoo.com>
2015-10-09 23:58:37 +0200

    extracted webservlet into its own class.

--- karchangame/src/main/java/awesomeness/vaadin/MyUI.java
+++ karchangame/src/main/java/awesomeness/vaadin/MyUI.java
@@ -1,15 +1,12 @@
 package awesomeness.vaadin;
 
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Accordion;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
-import javax.servlet.annotation.WebServlet;
 
 /**
  *
@@ -43,12 +40,4 @@ public class MyUI extends UI
         accordion.addTab(events, "Events");
     }
 
-    @WebServlet(urlPatterns =
-    {
-        "/administration/*", "/VAADIN/*"
-    }, name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet
-    {
-    }
 }
