75626ee2e9aebeb80796d87595f3e02441f26f17
Maarten van Leunen <maarten_l@yahoo.com>
2015-12-25 01:49:37 +0100

    /* removed "bugs" part */

--- sqlscripts/set_boarddescription.sql
+++ sqlscripts/set_boarddescription.sql
@@ -9,22 +9,14 @@ ${MYSQL_BIN} -h ${MYSQL_HOST} -u ${MYSQL_USR} --password=${MYSQL_PWD} -s ${MYSQL
 
 update mm_boards set description='<b>Logonmessage</b>
 <br>
-<img SRC="/images/gif/letters/w.gif"
-ALT="W" ALIGN=left>
+<img src="/images/gif/letters/w.gif" alt="W" align=left/>
 elcome to the Land of Karchan MUD, a land filled with mystery
 and enchantment, where weapons, magic, intelligence, and common
 sense play key roles in the realm. Where love and war can be one
 and the same. Where elves coexist peacefully with the humans, and
 make war with the dwarves. Where the sun rises, and the moon falls.
 Where one can change into a hero with a single swipe of his
-sword.<p>
-<A HREF="/scripts/bugs.php" target="_blank">
-<IMG ALIGN=right SRC="/images/gif/webpic/button.bugs.gif"
-BORDER="0"></A>
-Use the button you see on the right to register anything you 
-find wrong with the
-mud <I>or</I> anything you would like added.
-<P>' 
+sword.</p>' 
 where id=2;
 
 END_OF_DATA
