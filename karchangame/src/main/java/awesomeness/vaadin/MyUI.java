826c5d1ca80b71e7fbeb38861e7326ffa544e449
Maarten van Leunen <maarten_l@yahoo.com>
2016-01-06 21:40:16 +0100

    changed Tabs of administration pages into Menus for easier navigation.

--- karchangame/src/main/java/awesomeness/vaadin/MyUI.java
+++ karchangame/src/main/java/awesomeness/vaadin/MyUI.java
@@ -1,12 +1,30 @@
+/*
+ * Copyright (C) 2015 maartenl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package awesomeness.vaadin;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Layout;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.MenuBar.MenuItem;
 import com.vaadin.ui.Panel;
-import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import javax.persistence.EntityManager;
 
 /**
@@ -26,28 +44,98 @@ public class MyUI extends UI
         Panel mainPanel = new Panel("Administration pages");
         setContent(mainPanel);
 
+        VerticalLayout layout = new VerticalLayout();
+
+        mainPanel.setContent(layout);
+
         // Create the tabsheet
-        TabSheet tabsheet = new TabSheet();
-        mainPanel.setContent(tabsheet);
+        MenuBar barmenu = new MenuBar();
+        layout.addComponent(barmenu);
 
-        // Create the first tab, specify caption when adding
-        Layout worldattributes = new WorldAttributes(servlet.getWorldattributesProvider(), servlet.getCurrentUser(), servlet.getLogBean()); // Wrap in a layout
-        tabsheet.addTab(worldattributes, "World attributes");
+        final Panel worldAttributesPanel = new Panel("Worldattributes");
+        worldAttributesPanel.setVisible(false);
+        Layout worldattributes = new WorldAttributes(servlet.getWorldattributesProvider(), servlet.getCurrentUser(), servlet.getLogBean());
+        worldAttributesPanel.setContent(worldattributes);
+        layout.addComponent(worldAttributesPanel);
 
+        final Panel scriptsPanel = new Panel("Scripts");
+        scriptsPanel.setVisible(false);
         Layout scripts = new Scripts(servlet.getScriptsProvider(), servlet.getCurrentUser(), servlet.getLogBean());
-        tabsheet.addTab(scripts, "Scripts");
+        scriptsPanel.setContent(scripts);
+        layout.addComponent(scriptsPanel);
 
-        Layout areas = new Areas(servlet.getAreasProvider(), servlet.getCurrentUser(), servlet.getLogBean()); // Wrap in a layout
-        tabsheet.addTab(areas, "Areas");
+        final Panel areasPanel = new Panel("Areas");
+        areasPanel.setVisible(false);
+        Layout areas = new Areas(servlet.getAreasProvider(), servlet.getCurrentUser(), servlet.getLogBean());
+        areasPanel.setContent(areas);
+        layout.addComponent(areasPanel);
 
-        Layout events = new Events(servlet.getEventsProvider(), servlet.getCurrentUser(), servlet.getLogBean()); // Wrap in a layout
-        tabsheet.addTab(events, "Events");
+        final Panel eventsPanel = new Panel("Events");
+        eventsPanel.setVisible(false);
+        Layout events = new Events(servlet.getEventsProvider(), servlet.getCurrentUser(), servlet.getLogBean());
+        eventsPanel.setContent(events);
+        layout.addComponent(eventsPanel);
 
-        Layout rooms = new Rooms(servlet.getRoomsProvider(), servlet.getCurrentUser(), servlet.getLogBean()); // Wrap in a layout
-        tabsheet.addTab(rooms, "Rooms");
+        final Panel roomsPanel = new Panel("Rooms");
+        roomsPanel.setVisible(false);
+        Layout rooms = new Rooms(servlet.getRoomsProvider(), servlet.getCurrentUser(), servlet.getLogBean());
+        roomsPanel.setContent(rooms);
+        layout.addComponent(roomsPanel);
 
-        Layout commands = new Commands(servlet.getCommandsProvider(), servlet.getCurrentUser(), servlet.getLogBean()); // Wrap in a layout
-        tabsheet.addTab(commands, "Commands");
+        final Panel commandsPanel = new Panel("Commands");
+        commandsPanel.setVisible(false);
+        Layout commands = new Commands(servlet.getCommandsProvider(), servlet.getCurrentUser(), servlet.getLogBean());
+        commandsPanel.setContent(commands);
+        layout.addComponent(commandsPanel);
+        final MenuBar.Command command;
+        command = new MenuBar.Command()
+        {
+
+            @Override
+            public void menuSelected(MenuItem selectedItem)
+            {
+                worldAttributesPanel.setVisible(false);
+                scriptsPanel.setVisible(false);
+                areasPanel.setVisible(false);
+                eventsPanel.setVisible(false);
+                roomsPanel.setVisible(false);
+                commandsPanel.setVisible(false);
+
+                switch (selectedItem.getText())
+                {
+                    case "Worldattributes":
+                        worldAttributesPanel.setVisible(true);
+                        break;
+                    case "Scripts":
+                        scriptsPanel.setVisible(true);
+                        break;
+                    case "Areas":
+                        areasPanel.setVisible(true);
+                        break;
+                    case "Events":
+                        eventsPanel.setVisible(true);
+                        break;
+                    case "Rooms":
+                        roomsPanel.setVisible(true);
+                        break;
+                    case "Commands":
+                        commandsPanel.setVisible(true);
+                        break;
+                }
+            }
+        };
+
+        barmenu.addItem("Rooms", null, command);
+
+        barmenu.addItem("Areas", null, command);
+
+        // toplevel
+        MenuItem scripting = barmenu.addItem("Scripting", null, null);
+
+        scripting.addItem("Worldattributes", null, command);
+        scripting.addItem("Commands", null, command);
+        scripting.addItem("Events", null, command);
+        scripting.addItem("Scripts", null, command);
     }
 
 }
