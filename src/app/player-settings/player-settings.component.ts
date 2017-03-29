6ad047b3a9929712283fa9f4c9286bd760f3b61b
mrbear <maarten_l@yahoo.com>
2017-03-29 22:26:37 +0200

    /* added charactersheet info */

--- src/app/player-settings/player-settings.component.ts
+++ src/app/player-settings/player-settings.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-settings.component.css']
 })
 export class PlayerSettingsComponent implements OnInit {
-
-  constructor() { }
+  name: string;
+  title: string;
+  homepageUrl : string;
+  imageUrl: string;
+  date_of_birth: string;
+  city_of_birth: string;
+  
+  constructor() {
+    this.name = 'Karn';
+    this.title = 'Ruler of Karchan, Keeper of the Key to the Room of Lost Souls';
+    this.homepageUrl = 'http://www.karchan.org';
+    this.imageUrl = 'http://www.karchan.org/favico.ico';
+    this.date_of_birth = 'Sometime';
+    this.city_of_birth = 'The Dark';
+  }
 
   ngOnInit() {
   }
