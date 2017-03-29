b8f8e6f880160c5c9024777b7a6531c1d50a2f8f
mrbear <maarten_l@yahoo.com>
2017-03-29 22:07:43 +0200

    /* new "directive" is called a component in Angular 2. The new component is called player-settings, and is going to contain all the characteristics of a player and his/her avatar. */

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PlayerSettingsComponent } from './player-settings/player-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerSettingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/player-settings/player-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-settings',
+  templateUrl: './player-settings.component.html',
+  styleUrls: ['./player-settings.component.css']
+})
+export class PlayerSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
