aae4253773088d28d4e0079200ce628262f72601
mrbear <maarten_l@yahoo.com>
2017-04-03 14:35:38 +0200

    /* added tabs and made them clickable */

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
 
+export enum Tab {
+  Settings, Mail, Guild
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentTab : Tab = Tab.Settings;
+
+  Tab: Tab;
+
+  setSettingsTabActive() {
+    this.currentTab = Tab.Settings;
+  }
+
+  isSettingsTabActive() {
+    return this.currentTab === Tab.Settings;
+  }
+
+  setMailTabActive() {
+    this.currentTab = Tab.Mail;
+  }
+
+  isMailTabActive() {
+    return this.currentTab === Tab.Mail;
+  }
+
+  setGuildTabActive() {
+    this.currentTab = Tab.Guild;
+  }
+
+  isGuildTabActive() {
+    return this.currentTab === Tab.Guild;
+  }
 }
