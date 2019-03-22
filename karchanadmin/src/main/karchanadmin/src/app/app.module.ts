5030e4bb9cc1ad45771a36c9dd755ba3f7f0d741
Maarten van Leunen <maartenl@yahoo.com>
2019-03-22 09:17:59 +0100

    /* two new components added : blogs and templates, first attempt */

--- karchanadmin/src/main/karchanadmin/src/app/app.module.ts
+++ karchanadmin/src/main/karchanadmin/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { TemplatesComponent } from './templates/templates.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogsComponent,
+    TemplatesComponent
   ],
   imports: [
     BrowserModule,
---
+++ karchanadmin/src/main/karchanadmin/src/app/blogs/blogs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blogs',
+  templateUrl: './blogs.component.html',
+  styleUrls: ['./blogs.component.css']
+})
+export class BlogsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
