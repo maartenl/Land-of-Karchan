8a78ebb00fb860f24951d0e33445da47aa3a92e9
Maarten van Leunen <maartenl@yahoo.com>
2019-03-22 09:47:14 +0100

    /* added routes to the two components, changed the html to remove the components as they are automatically rendered based on the route, in the router-outlet tag. */

--- karchanadmin/src/main/karchanadmin/src/app/app-routing.module.ts
+++ karchanadmin/src/main/karchanadmin/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
+import { TemplatesComponent } from './templates/templates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'templates', component: TemplatesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
