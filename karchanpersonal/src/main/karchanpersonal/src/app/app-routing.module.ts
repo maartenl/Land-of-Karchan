b517111d0325d7cc68bff8e4cc6a1c2c3e5cf519
Maarten van Leunen <maarten_l@yahoo.com>
2019-07-19 00:30:05 +0200

    /* added routing properly to karchanpersonal */

--- karchanpersonal/src/main/karchanpersonal/src/app/app-routing.module.ts
+++ karchanpersonal/src/main/karchanpersonal/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GuildComponent } from './guild/guild.component';
+import { MailComponent } from './mail/mail.component';
+import { PlayerSettingsComponent } from './player-settings/player-settings.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'settings', component: PlayerSettingsComponent },
+  { path: 'mail', component: MailComponent },
+  { path: 'guild', component: GuildComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
