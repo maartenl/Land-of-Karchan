0efd47cb697829634c2fb323e21fcbd9190aafde
Maarten van Leunen <maarten_l@yahoo.com>
2019-07-19 23:21:57 +0200

    /* facilitate deep linking by redirecting sub-urls to the main index.html and letting Angular deal with it. For example: http://www.karchan.org/karchanpersonal-2.0.2/mail will immediately show your mail. */

---
+++ karchanadmin/src/main/java/com/karchan/RedirectingServlet.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2019 maartenl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.karchan;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Takes care to send the index.html file of Angular, so Angular routing can be
+ * done with bookmarks.
+ *
+ * @author maartenl
+ */
+@WebServlet(name = "RedirectingServlet", urlPatterns =
+{
+  "/blogs/*", "/templates/*"
+})
+public class RedirectingServlet extends HttpServlet
+{
+
+  /**
+   * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+   * methods.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    response.setContentType("text/html;charset=UTF-8");
+    try (ServletOutputStream out = response.getOutputStream();
+            InputStream in = request.getServletContext().getResourceAsStream("/index.html"))
+    {
+      byte[] buffer = new byte[1024];
+      int len = in.read(buffer);
+      while (len != -1)
+      {
+        out.write(buffer, 0, len);
+        len = in.read(buffer);
+      }
+    }
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /**
+   * Handles the HTTP <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException
+  {
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo()
+  {
+    return "Short description";
+  }// </editor-fold>
+
+}
