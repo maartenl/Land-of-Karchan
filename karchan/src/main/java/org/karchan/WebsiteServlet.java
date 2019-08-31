8dd675b5a1028127eb1ced3ab88b3d49ea9ccbd9
Maarten van Leunen <maarten_l@yahoo.com>
2019-09-01 01:49:15 +0200

    /* new release 2.0.3 */

--- karchan/src/main/java/org/karchan/WebsiteServlet.java
+++ karchan/src/main/java/org/karchan/WebsiteServlet.java
@@ -57,7 +57,7 @@ public class WebsiteServlet extends HttpServlet
 
   private static final String VERSION_PARAMETER = "version";
 
-  private static final String CURRENT_VERSION = "2.0.3-SNAPSHOT";
+  private static final String CURRENT_VERSION = "2.0.3";
 
   @Inject
   private SecurityContext securityContext;
