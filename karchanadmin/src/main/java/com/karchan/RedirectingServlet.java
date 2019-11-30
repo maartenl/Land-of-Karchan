59fc47cdf6eb06de3c38a49a198c3a74c745f1dc
Maarten van Leunen <maarten_l@yahoo.com>
2019-11-30 16:33:31 +0100

    /* fixed redirect to root from /methods/*. updated new angular */

--- karchanadmin/src/main/java/com/karchan/RedirectingServlet.java
+++ karchanadmin/src/main/java/com/karchan/RedirectingServlet.java
@@ -34,7 +34,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "RedirectingServlet", urlPatterns =
 {
-  "/blogs/*", "/templates/*", "/systemlog/*", "/rooms/*"
+  "/blogs/*", "/templates/*", "/systemlog/*", "/rooms/*", "/methods/*"
 })
 public class RedirectingServlet extends HttpServlet
 {
