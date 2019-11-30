0361d45eb20f4dffbe3522528c0b0da6647681b1
Maarten van Leunen <maartenl@yahoo.com>
2019-11-30 14:40:32 +0100

    /* added christmas */

--- karchanadmin/src/main/karchanadmin/src/app/app.component.ts
+++ karchanadmin/src/main/karchanadmin/src/app/app.component.ts
@@ -20,4 +20,18 @@ export class AppComponent implements OnInit {
     }
   }
 
+//  isChristmas(currentDate: Date = new Date()): boolean {
+  isChristmas(currentDate: Date = new Date(2019, 11, 27)): boolean {
+    const beforeChristmas = new Date(currentDate.getFullYear(), 11, 7).getTime();
+    const afterChristmas = new Date(currentDate.getFullYear() + 1, 0, 6).getTime();
+    return beforeChristmas < currentDate.getTime() && afterChristmas > currentDate.getTime();
+  }
+
+  getFavicon(): string {
+    if (this.isChristmas()) {
+      return 'assets/images/santadragon.gif';
+    }
+    return 'assets/images/dragon.gif';
+  }
+
 }
--- karchanpersonal/src/main/karchanpersonal/src/app/app.component.ts
+++ karchanpersonal/src/main/karchanpersonal/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
   constructor(
     private cookieService: CookieService) {
     this.production = environment.production;
-    if (window.console) {console.log('Production: ' + this.production);}
+    if (window.console) { console.log('Production: ' + this.production); }
   }
 
   ngOnInit(): void {
@@ -25,4 +25,17 @@ export class AppComponent implements OnInit {
     }
   }
 
+  //  isChristmas(currentDate: Date = new Date()): boolean {
+  isChristmas(currentDate: Date = new Date(2019, 11, 27)): boolean {
+    const beforeChristmas = new Date(currentDate.getFullYear(), 11, 7).getTime();
+    const afterChristmas = new Date(currentDate.getFullYear() + 1, 0, 6).getTime();
+    return beforeChristmas < currentDate.getTime() && afterChristmas > currentDate.getTime();
+  }
+
+  getFavicon(): string {
+    if (this.isChristmas()) {
+      return 'assets/images/santadragon.gif';
+    }
+    return 'assets/images/dragon.gif';
+  }
 }
