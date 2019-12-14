606cd9c5ebb80d8a1de3e7aaa9075707c1a15af0
Maarten van Leunen <maarten_l@yahoo.com>
2019-12-14 23:55:18 +0100

    /* added help function tot the karchan admin pages. The help function redirects to teh wikipages in a new tab. */

--- karchanadmin/src/main/karchanadmin/src/app/app.component.ts
+++ karchanadmin/src/main/karchanadmin/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
 import { CookieService } from 'ngx-cookie-service';
 import { ChristmasUtils } from './christmas.utils';
 
@@ -10,7 +12,9 @@ import { ChristmasUtils } from './christmas.utils';
 export class AppComponent implements OnInit {
   darkmode: boolean;
 
-  constructor(private cookieService: CookieService) {
+  constructor(
+    private cookieService: CookieService,
+    private router: Router) {
   }
 
   ngOnInit(): void {
@@ -27,4 +31,20 @@ export class AppComponent implements OnInit {
     }
     return 'assets/images/dragon.gif';
   }
+
+  getHelpLink(): string {
+    const url = this.router.url.substring(1);
+    if (url.indexOf('/') === -1) {
+      if (url === '') {
+        // this.router.url is for example /
+        return '/wiki/Admin Pages.html';
+      }
+      // this.router.url is for example /commands
+      return '/wiki/Admin' + url + '.html';
+    }
+    // this.router.url is for example /commands/15
+    const wikiUrl = url.substring(0, url.indexOf('/'));
+    return '/wiki/Admin' + wikiUrl + '.html';
+  }
+
 }
