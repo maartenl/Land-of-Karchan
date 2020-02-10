1b2216bb5fdd8f26c10cd3d961bd021c066c306a
Maarten van Leunen <maartenl@yahoo.com>
2020-02-10 17:45:55 +0100

    /* fix christmas errors in the apps */

--- karchanadmin/src/main/karchanadmin/src/app/christmas.utils.ts
+++ karchanadmin/src/main/karchanadmin/src/app/christmas.utils.ts
@@ -3,7 +3,7 @@ export class ChristmasUtils {
     public static isChristmas(currentDate: Date = new Date()): boolean {
         // isChristmas(currentDate: Date = new Date(2019, 11, 27)): boolean {
         const beforeChristmas = new Date(currentDate.getFullYear(), 11, 7).getTime();
-        const afterChristmas = new Date(currentDate.getFullYear() + 1, 0, 6).getTime();
-        return beforeChristmas < currentDate.getTime() && afterChristmas > currentDate.getTime();
+        const afterChristmas = new Date(currentDate.getFullYear(), 0, 6).getTime();
+        return beforeChristmas < currentDate.getTime() || afterChristmas > currentDate.getTime();
     }
 }
