0f891f4b197b709a988a8ef7cc59d840e4211c78
Maarten van Leunen <maarten_l@yahoo.com>
2020-05-21 14:59:23 +0200

    /* added boards to the redirecting servlet */

--- karchanadmin/src/main/java/com/karchan/RedirectingServlet.java
+++ karchanadmin/src/main/java/com/karchan/RedirectingServlet.java
@@ -34,7 +34,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "RedirectingServlet", urlPatterns =
 {
-  "/blogs/*", "/templates/*", "/systemlog/*", "/rooms/*", "/methods/*", "/commands/*", "/areas/*", "/items/*", "/ban/*", "/manpages/*", "/events/*",  "/worldattributes/*", "/characters/*"
+  "/blogs/*", "/templates/*", "/systemlog/*", "/rooms/*", "/methods/*", "/commands/*", "/areas/*", "/items/*", "/ban/*", "/manpages/*", "/events/*",  "/worldattributes/*", "/characters/*", "/boards/*"
 })
 public class RedirectingServlet extends HttpServlet
 {
