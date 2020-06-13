79462f265af3a51546a6fd397ec90638a365561e
Maarten van Leunen <maarten_l@yahoo.com>
2020-06-13 11:41:17 +0200

    /* upgrade to 2.0.7-SNAPSHOT */

--- karchan/src/main/java/org/karchan/WebsiteServlet.java
+++ karchan/src/main/java/org/karchan/WebsiteServlet.java
@@ -57,7 +57,7 @@ public class WebsiteServlet extends HttpServlet
 
   private static final String VERSION_COOKIENAME = "karchanversion";
 
-  private static final String CURRENT_VERSION = "2.0.6";
+  private static final String CURRENT_VERSION = "2.0.7-SNAPSHOT";
 
   @Inject
   private SecurityContext securityContext;
