1a6e6ddffc95cc137711bf0d3bd91e440985993f
Maarten van Leunen <maarten_l@yahoo.com>
2022-10-13 12:25:51 +0200

    /* upgrade to 2.0.11-SNAPSHOT */

--- karchan/src/main/java/org/karchan/WebsiteServlet.java
+++ karchan/src/main/java/org/karchan/WebsiteServlet.java
@@ -60,7 +60,7 @@ public class WebsiteServlet extends HttpServlet
 
   private static final String VERSION_COOKIENAME = "karchanversion";
 
-  private static final String CURRENT_VERSION = "2.0.10-SNAPSHOT";
+  private static final String CURRENT_VERSION = "2.0.11-SNAPSHOT";
 
   /**
    * For example: https://www.karchan.org. If it isn't configured, then no redirect takes place.
